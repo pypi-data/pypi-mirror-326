94aa0a7a18332f889d3358f0dc53d6890e8b2c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:07 +0000

    Add package inspect-ai/0.3.63/inspect_ai-0.3.63-py3-none-any.whl

---
+++ packages/inspect-ai/inspect_ai-0.3.63-py3-none-any.whl/inspect_ai/scorer/_metrics/__init__.py
@@ -0,0 +1,12 @@
+from .accuracy import accuracy
+from .mean import mean, var
+from .std import bootstrap_stderr, std, stderr
+
+__all__ = [
+    "accuracy",
+    "mean",
+    "var",
+    "bootstrap_stderr",
+    "std",
+    "stderr",
+]
