d5ae153e8f8181a20a3652ac42ae3097f6bd4e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:30:16 +0000

    Add package nillion-signatures/0.1.2/nillion_signatures-0.1.2.tar.gz

---
+++ packages/nillion-signatures/nillion_signatures-0.1.2.tar.gz/nillion_signatures-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nillion-signatures",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography>=3.4.7",
+        "nilql>=0.0.0a9",
+        "requests>=2.32.3"
+    ],
+    author="Dave Butler",
+    author_email="david.butler@nillion.com",
+    description="A signature library to perform key generation and signing on the Nillion Network",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NillionNetwork/signatures-py",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
