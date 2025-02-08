92ed62e142b4d6c363d4ef629a4f78b8c26a79b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:17:29 +0000

    Add package moose-lib/0.3.781/moose_lib-0.3.781-py3-none-any.whl

---
+++ packages/moose-lib/moose_lib-0.3.781-py3-none-any.whl/moose_lib/tasks.py
@@ -0,0 +1,18 @@
+from functools import wraps
+from typing import TypeVar, Callable, Any
+from .commons import Logger
+
+T = TypeVar('T')
+
+def task(func: Callable[..., T]) -> Callable[..., T]:
+    """Decorator to mark a function as a Moose task"""
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> T:
+        # Mark this function as a Moose task
+        wrapper._is_moose_task = True
+        wrapper.logger = Logger(is_moose_task=True)
+        return func(*args, **kwargs)
+    
+    # Add the marker to the wrapper
+    wrapper._is_moose_task = True
+    return wrapper
