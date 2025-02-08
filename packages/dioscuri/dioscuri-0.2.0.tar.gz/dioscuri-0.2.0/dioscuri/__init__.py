d6494cd704f8eeae47057536ebad0aa0410e7c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:05:24 +0000

    Add package dioscuri/0.2.0/dioscuri-0.2.0.tar.gz

---
+++ packages/dioscuri/dioscuri-0.2.0.tar.gz/dioscuri-0.2.0/dioscuri/__init__.py
@@ -0,0 +1,16 @@
+from .dioscuri import (
+    GeminiWorkList,
+    Pipette,
+    WashTipOrReplaceDITI,
+    Decontamination,
+    Flush,
+    Break,
+    SetDITIType,
+    Comment,
+    ReagentDistribution,
+    StartTimer,
+    WaitForTimer,
+    read_gwl,
+)
+
+from .version import __version__
---
+++ packages/dioscuri/dioscuri-0.2.0.tar.gz/dioscuri-0.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+version = {}
+with open("dioscuri/version.py") as fp:
+    exec(fp.read(), version)
+
+setup(
+    name="dioscuri",
+    version=version["__version__"],
+    author="Peter Vegh",
+    description="Read/write Gemini WorkList (gwl) files.",
+    long_description=open("pypi-readme.rst").read(),
+    long_description_content_type="text/x-rst",
+    license="MIT",
+    url="https://github.com/Edinburgh-Genome-Foundry/Dioscuri",
+    keywords="biology",
+    packages=find_packages(exclude="docs"),
+)
