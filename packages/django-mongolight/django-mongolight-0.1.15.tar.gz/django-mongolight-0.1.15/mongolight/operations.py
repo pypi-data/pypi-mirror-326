e84c4b967224968eade254d14b274f95c09b3553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:19 +0000

    Add package django-mongolight/0.1.15/django-mongolight-0.1.15.tar.gz

---
+++ packages/django-mongolight/django-mongolight-0.1.15.tar.gz/django-mongolight-0.1.15/mongolight/operations.py
@@ -0,0 +1,22 @@
+from django.db.backends.base.operations import BaseDatabaseOperations
+
+
+class DatabaseOperations(BaseDatabaseOperations):
+    compiler_module = "mongolight.compiler"
+
+    def sql_flush(self, style, tables, reset_sequences, allow_cascade=False):
+        for table in tables:
+            self.connection[table].delete_many({})
+        return []
+
+    def max_name_length(self):
+        """
+        Return the maximum length of table and column names.
+        """
+        return 255  # MongoDB no tiene un límite estricto, pero Django necesita un valor
+
+    def quote_name(self, name):
+        """
+        Return the name as-is, since MongoDB does not require quoting.
+        """
+        return name
---
+++ packages/django-mongolight/django-mongolight-0.1.15.tar.gz/django-mongolight-0.1.15/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-mongolight",
+    version="0.1.15",
+    author="Bryan Jesus Ramon Avila",
+    author_email="bryanjesus.ra@gmail.com",
+    description="A lightweight library for integrating Django with MongoDB.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bryancitu/django-mongolight",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "Django >= 4.0",
+        "pymongo >= 4.0",
+    ],
+)
