d3b49ab5b5e77a99c76d1f9b88718dcc63a21cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:03:55 +0000

    Add package fastcrawl/0.3.0/fastcrawl-0.3.0.tar.gz

---
+++ packages/fastcrawl/fastcrawl-0.3.0.tar.gz/fastcrawl-0.3.0/fastcrawl/models/crawler_settings.py
@@ -0,0 +1,34 @@
+from typing import Annotated
+
+from dotenv import find_dotenv
+from pydantic.functional_serializers import PlainSerializer
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from fastcrawl.base_pipeline import BasePipeline
+from fastcrawl.models.http_client_settings import HttpClientSettings
+from fastcrawl.models.log_settings import LogSettings
+
+
+class CrawlerSettings(BaseSettings):
+    """Crawler settings model.
+
+    Attributes:
+        workers (int): Number of workers to process requests. Default is 15.
+        pipelines (list[type[BasePipeline]]): List of pipelines to process items.
+            Pipelines will be executed in the order they are defined. Default is [].
+        log (LogSettings): Log settings for the crawler. Default is LogSettings().
+        http_client (HttpClientSettings): HTTP client settings for the crawler. Default is HttpClientSettings().
+
+    """
+
+    workers: int = 15
+    pipelines: list[Annotated[type[BasePipeline], PlainSerializer(lambda x: x.__name__)]] = []
+    log: LogSettings = LogSettings()
+    http_client: HttpClientSettings = HttpClientSettings()
+
+    model_config = SettingsConfigDict(
+        env_file=find_dotenv(),
+        env_prefix="fastcrawl_",
+        env_nested_delimiter="__",
+        extra="ignore",
+    )
