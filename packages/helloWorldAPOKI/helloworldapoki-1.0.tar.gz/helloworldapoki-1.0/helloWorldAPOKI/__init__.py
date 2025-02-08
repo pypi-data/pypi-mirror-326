fbbf1f85dd2a528cd6442b0fd93df3d0a3ba629e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:03:38 +0000

    Add package helloWorldAPOKI/1.0/helloworldapoki-1.0.tar.gz

---
+++ packages/helloWorldAPOKI/helloworldapoki-1.0.tar.gz/helloworldapoki-1.0/helloWorldAPOKI/__init__.py
@@ -0,0 +1,35 @@
+from .functions import (
+    helloWorld,
+    helloWorld_up,
+    helloWorld_down,
+    helloWorld_countDig,
+    helloWorld_reverse,
+    helloWorld_palindrome,
+    helloWorld_countWrds,
+    helloWorld_spaces,
+    helloWorld_vow,
+    helloWorld_fAndL,
+    helloWorld_title,
+    helloWorld_swapCase,
+    helloWorld_punct,
+    helloWorld_longest,
+    helloWorld_SCletter
+)
+
+__all__ = [
+    "helloWorld",
+    "helloWorld_up",
+    "helloWorld_down",
+    "helloWorld_countDig",
+    "helloWorld_reverse",
+    "helloWorld_palindrome",
+    "helloWorld_countWrds",
+    "helloWorld_spaces",
+    "helloWorld_vow",
+    "helloWorld_fAndL",
+    "helloWorld_title",
+    "helloWorld_swapCase",
+    "helloWorld_punct",
+    "helloWorld_longest",
+    "helloWorld_SCletter"
+]
