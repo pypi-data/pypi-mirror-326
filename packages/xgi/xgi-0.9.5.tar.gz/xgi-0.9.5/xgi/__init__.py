27699e2bbef31e4719cabe644c6088b33a434f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:05:43 +0000

    Add package xgi/0.9.5/xgi-0.9.5.tar.gz

---
+++ packages/xgi/xgi-0.9.5.tar.gz/xgi-0.9.5/xgi/__init__.py
@@ -0,0 +1,25 @@
+from . import (
+    utils,
+    core,
+    algorithms,
+    convert,
+    drawing,
+    dynamics,
+    generators,
+    linalg,
+    readwrite,
+    stats,
+)
+from .utils import *
+from .core import *
+from .algorithms import *
+from .convert import *
+from .drawing import *
+from .dynamics import *
+from .generators import *
+from .linalg import *
+from .readwrite import *
+from .stats import *
+
+
+__version__ = "0.9.5"
