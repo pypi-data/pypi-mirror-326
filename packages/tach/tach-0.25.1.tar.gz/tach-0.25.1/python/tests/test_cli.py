b804d55f3eaeb8f3ac1feeba0fc9d5fee73377d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 14:03:50 +0000

    Add package tach/0.25.1/tach-0.25.1.tar.gz

---
+++ packages/tach/tach-0.25.1.tar.gz/tach-0.25.1/python/tests/test_cli.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from pathlib import Path
+from unittest.mock import Mock
+
+import pytest
+
+from tach import cli
+from tach.extension import ProjectConfig
+
+
+@pytest.fixture
+def mock_check(mocker) -> Mock:
+    mock = Mock(return_value=[])  # default to a return with no errors
+    mocker.patch("tach.cli.check", mock)
+    return mock
+
+
+@pytest.fixture
+def mock_project_config(mocker) -> ProjectConfig:
+    def _mock_project_config(root: str = "") -> ProjectConfig:
+        return ProjectConfig()
+
+    mocker.patch("tach.cli.parse_project_config", _mock_project_config)
+    return _mock_project_config()
+
+
+def test_execute_with_config(capfd, mock_check, mock_project_config):
+    # Test with a valid path as mocked
+    args, _ = cli.parse_arguments(["check"])
+    assert args.command == "check"
+    with pytest.raises(SystemExit) as sys_exit:
+        cli.tach_check(
+            project_root=Path(),
+            project_config=mock_project_config,
+        )
+    captured = capfd.readouterr()
+    assert sys_exit.value.code == 0
+    assert "✅" in captured.out
+    assert "All modules validated!" in captured.out
+
+
+def test_invalid_command(capfd):
+    with pytest.raises(SystemExit) as sys_exit:
+        # Test with an invalid command
+        cli.parse_arguments(["help"])
+    captured = capfd.readouterr()
+    assert sys_exit.value.code == 2
+    assert "invalid choice: 'help" in captured.err
+
+
+def test_execute_with_valid_exclude(capfd, mock_check, mock_project_config):
+    with pytest.raises(SystemExit) as sys_exit:
+        # Test with a valid path as mocked
+        args, _ = cli.parse_arguments(["check", "--exclude", "valid_dir"])
+        mock_project_config.exclude = args.exclude.split(",")
+        cli.tach_check(
+            project_root=Path(),
+            project_config=mock_project_config,
+        )
+    captured = capfd.readouterr()
+    assert sys_exit.value.code == 0
+    assert "✅" in captured.out
+    assert "All modules validated!" in captured.out
