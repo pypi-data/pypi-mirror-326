8bf580d59d47e66d340e0f3a88074eb1c36c2f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:15 +0000

    Add package igrf-model/1.1.1/igrf-model-1.1.1.tar.gz

---
+++ packages/igrf-model/igrf-model-1.1.1.tar.gz/igrf-model-1.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['igrf_model', 'igrf_model.data']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'igrf-model',
+    'version': '1.1.1',
+    'description': 'Pure Python implementation of the IGRF13 magnetic model',
+    'long_description': None,
+    'author': 'Tamas Nepusz',
+    'author_email': 'ntamas@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
