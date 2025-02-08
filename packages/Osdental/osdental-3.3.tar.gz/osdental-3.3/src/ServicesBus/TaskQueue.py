53ba0de08f975624f04faff3ea018283dff71ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 14:14:09 +0000

    Add package Osdental/3.3/osdental-3.3.tar.gz

---
+++ packages/Osdental/osdental-3.3.tar.gz/osdental-3.3/src/ServicesBus/TaskQueue.py
@@ -0,0 +1,43 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+from ServicesBus.ServicesBus import ServicesBus
+
+load_dotenv()
+
+class TaskQueue:
+    """Queue to manage tasks in order and asynchronously."""
+
+    def __init__(self):
+        self.conn_str = os.getenv('CONNECTION_STRING')
+        self.queue_name = os.getenv('QUEUE')
+        self.service_bus = ServicesBus(self.conn_str, self.queue_name)
+        self.queue = asyncio.Queue()
+        
+
+    def start_processing(self) -> None:
+        """Start processing tasks in the background."""
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+
+        loop.create_task(self.process_tasks())
+
+
+    async def enqueue(self, message: dict) -> None:
+        """Add a task to the queue."""
+        await self.queue.put(message)
+
+
+    async def process_tasks(self) -> None:
+        """Process tasks from the queue in order."""
+        while True:
+            message = await self.queue.get()
+            try:
+                await self.service_bus.send_message(message)
+            except Exception as e:
+                raise ValueError(f'Message queuing error: {e} | Message: {message}')
+            finally:
+                self.queue.task_done()
