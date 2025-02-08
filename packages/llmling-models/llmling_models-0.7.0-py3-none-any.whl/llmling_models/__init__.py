3be9992da05b43bfd2a1e8195f15948d721009e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:25:54 +0000

    Add package llmling-models/0.7.0/llmling_models-0.7.0-py3-none-any.whl

---
+++ packages/llmling-models/llmling_models-0.7.0-py3-none-any.whl/llmling_models/__init__.py
@@ -0,0 +1,34 @@
+__version__ = "0.7.0"
+
+
+from llmling_models.base import PydanticModel
+from llmling_models.multi import MultiModel
+from llmling_models.inputmodel import InputModel
+from llmling_models.importmodel import ImportModel
+from llmling_models.input_handlers import DefaultInputHandler
+from llmling_models.multimodels import (
+    FallbackMultiModel,
+    TokenOptimizedMultiModel,
+    CostOptimizedMultiModel,
+    DelegationMultiModel,
+    UserSelectModel,
+)
+from llmling_models.utils import infer_model
+from llmling_models.model_types import AllModels, ModelInput, AnyModel
+
+__all__ = [
+    "AllModels",
+    "AnyModel",
+    "CostOptimizedMultiModel",
+    "DefaultInputHandler",
+    "DelegationMultiModel",
+    "FallbackMultiModel",
+    "ImportModel",
+    "InputModel",
+    "ModelInput",
+    "MultiModel",
+    "PydanticModel",
+    "TokenOptimizedMultiModel",
+    "UserSelectModel",
+    "infer_model",
+]
