44187ee5669a7473d64f17c9b8532755085cb685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:22 +0000

    Add package med-imagetools/1.23.0/med_imagetools-1.23.0-py3-none-any.whl

---
+++ packages/med-imagetools/med_imagetools-1.23.0-py3-none-any.whl/imgtools/coretypes/__init__.py
@@ -0,0 +1,4 @@
+from .box import BoxPadMethod, RegionBox
+from .spatial_types import Coordinate3D, Size3D, Spacing3D
+
+__all__ = ["Coordinate3D", "Size3D", "Spacing3D", "RegionBox", "BoxPadMethod"]
