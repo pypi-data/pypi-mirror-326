00232c2e0312ebbf9941adba6d85d61787729443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:25 +0000

    Add package social-sdk-ti-ti/0.0.1/social_sdk_ti_ti-0.0.1-py3-none-any.whl

---
+++ packages/social-sdk-ti-ti/social_sdk_ti_ti-0.0.1-py3-none-any.whl/social_sdk_ti_ti/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from social_sdk_ti_ti.api.brand_api import BrandApi
+from social_sdk_ti_ti.api.generation_api import GenerationApi
+from social_sdk_ti_ti.api.healthcheck_api import HealthcheckApi
+from social_sdk_ti_ti.api.prompt_api import PromptApi
+from social_sdk_ti_ti.api.source_api import SourceApi
+from social_sdk_ti_ti.api.swagger_api import SwaggerApi
+from social_sdk_ti_ti.api.tenant_api import TenantApi
+from social_sdk_ti_ti.api.worker_api import WorkerApi
+from social_sdk_ti_ti.api.default_api import DefaultApi
+
