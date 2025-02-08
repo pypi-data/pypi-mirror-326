1280764a53f40499ce6db0db0668ce2781c4b3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 14:14:46 +0000

    Add package dandy/0.9.0/dandy-0.9.0-py3-none-any.whl

---
+++ packages/dandy/dandy-0.9.0-py3-none-any.whl/dandy/llm/service/request/openai.py
@@ -0,0 +1,33 @@
+from typing_extensions import Union
+
+from dandy.llm.service.request.request import BaseRequestBody
+
+
+_JSON_RESPONSE_FORMAT = {'type': 'json_object'}
+_TEXT_RESPONSE_FORMAT = {'type': 'text'}
+
+
+class OpenaiRequestBody(BaseRequestBody):
+    stream: bool = False
+    response_format: dict = _JSON_RESPONSE_FORMAT
+    max_completion_tokens: Union[int, None] = None
+    seed: Union[int, None] = None
+    temperature: Union[float, None] = None
+
+    def get_context_length(self) -> int:
+        return 0
+
+    def get_max_completion_tokens(self) -> int:
+        return self.max_completion_tokens
+
+    def get_seed(self) -> int:
+        return self.seed
+
+    def get_temperature(self) -> float:
+        return self.temperature
+
+    def set_format_to_json(self):
+        self.response_format = _JSON_RESPONSE_FORMAT
+
+    def set_format_to_text(self):
+        self.response_format = _TEXT_RESPONSE_FORMAT
