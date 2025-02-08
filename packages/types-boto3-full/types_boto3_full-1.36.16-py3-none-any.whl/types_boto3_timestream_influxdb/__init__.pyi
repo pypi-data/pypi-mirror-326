d662b6395df7d48a473ebef8f89efe43b336eae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 14:14:36 +0000

    Add package types-boto3-full/1.36.16/types_boto3_full-1.36.16-py3-none-any.whl

---
+++ packages/types-boto3-full/types_boto3_full-1.36.16-py3-none-any.whl/types_boto3_timestream_influxdb/__init__.pyi
@@ -0,0 +1,37 @@
+"""
+Main interface for timestream-influxdb service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_timestream_influxdb/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_timestream_influxdb import (
+        Client,
+        ListDbInstancesPaginator,
+        ListDbParameterGroupsPaginator,
+        TimestreamInfluxDBClient,
+    )
+
+    session = Session()
+    client: TimestreamInfluxDBClient = session.client("timestream-influxdb")
+
+    list_db_instances_paginator: ListDbInstancesPaginator = client.get_paginator("list_db_instances")
+    list_db_parameter_groups_paginator: ListDbParameterGroupsPaginator = client.get_paginator("list_db_parameter_groups")
+    ```
+"""
+
+from .client import TimestreamInfluxDBClient
+from .paginator import ListDbInstancesPaginator, ListDbParameterGroupsPaginator
+
+Client = TimestreamInfluxDBClient
+
+__all__ = (
+    "Client",
+    "ListDbInstancesPaginator",
+    "ListDbParameterGroupsPaginator",
+    "TimestreamInfluxDBClient",
+)
