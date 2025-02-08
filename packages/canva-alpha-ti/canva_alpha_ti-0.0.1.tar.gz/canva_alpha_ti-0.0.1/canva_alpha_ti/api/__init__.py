62038241110b3cbfec2eb70873ce46331e16a4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:05:16 +0000

    Add package canva-alpha-ti/0.0.1/canva_alpha_ti-0.0.1.tar.gz

---
+++ packages/canva-alpha-ti/canva_alpha_ti-0.0.1.tar.gz/canva_alpha_ti-0.0.1/canva_alpha_ti/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from canva_alpha_ti.api.app_api import AppApi
+from canva_alpha_ti.api.asset_api import AssetApi
+from canva_alpha_ti.api.autofill_api import AutofillApi
+from canva_alpha_ti.api.brand_template_api import BrandTemplateApi
+from canva_alpha_ti.api.comment_api import CommentApi
+from canva_alpha_ti.api.connect_api import ConnectApi
+from canva_alpha_ti.api.design_api import DesignApi
+from canva_alpha_ti.api.design_import_api import DesignImportApi
+from canva_alpha_ti.api.export_api import ExportApi
+from canva_alpha_ti.api.folder_api import FolderApi
+from canva_alpha_ti.api.oauth_api import OauthApi
+from canva_alpha_ti.api.user_api import UserApi
+
