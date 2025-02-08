fe19048c1fd7abb4126d013db6be1da7a2c27252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:05:38 +0000

    Add package pynspd/0.5.4/pynspd-0.5.4.tar.gz

---
+++ packages/pynspd/pynspd-0.5.4.tar.gz/pynspd-0.5.4/src/pynspd/__init__.py
@@ -0,0 +1,8 @@
+from pynspd._async.api import AsyncNspd
+from pynspd._sync.api import Nspd
+from pynspd.errors import UnknownLayer
+from pynspd.schemas import NspdFeature
+from pynspd.types.enums import ThemeId
+
+__all__ = ["AsyncNspd", "Nspd", "NspdFeature", "ThemeId", "UnknownLayer"]
+__version__ = "0.5.4"
---
+++ packages/pynspd/pynspd-0.5.4.tar.gz/pynspd-0.5.4/src/pynspd/asyncio_mock.py
@@ -0,0 +1,6 @@
+import typing_extensions
+
+
+@typing_extensions.deprecated("Will be removed in 0.6.0")
+def gather(*args):
+    return args
