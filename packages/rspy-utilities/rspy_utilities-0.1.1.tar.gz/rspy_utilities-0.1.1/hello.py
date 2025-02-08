b7da84dbd2c384aa62ea3c0cdd570add663a9a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:03:36 +0000

    Add package rspy-utilities/0.1.1/rspy_utilities-0.1.1.tar.gz

---
+++ packages/rspy-utilities/rspy_utilities-0.1.1.tar.gz/rspy_utilities-0.1.1/hello.py
@@ -0,0 +1,30 @@
+from rspy_utilities import distinct, partition
+
+print(list(distinct([1, 2, 2, 3, 3, 4])))  # [1, 2, 3, 4]
+
+for i in distinct(range(1000), lambda x: x % 10):
+    print(i)
+
+iterator = distinct(range(10), lambda x: x % 10)
+
+assert next(iterator) == 0
+assert next(iterator) == 1
+assert next(iterator) == 2
+assert next(iterator) == 3
+assert next(iterator) == 4
+assert next(iterator) == 5
+assert next(iterator) == 6
+assert next(iterator) == 7
+assert next(iterator) == 8
+assert next(iterator) == 9
+
+raised = False
+try:
+    next(iterator)
+except StopIteration:
+    raised = True
+
+assert raised
+
+
+print(partition(range(10), lambda x: x % 2 == 0))
