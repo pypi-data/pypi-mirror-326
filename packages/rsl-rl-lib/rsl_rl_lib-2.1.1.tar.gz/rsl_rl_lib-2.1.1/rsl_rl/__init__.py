13c2afeedf852170aa8ec4155ab172940ea2c55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:07 +0000

    Add package rsl-rl-lib/2.1.1/rsl_rl_lib-2.1.1.tar.gz

---
+++ packages/rsl-rl-lib/rsl_rl_lib-2.1.1.tar.gz/rsl_rl_lib-2.1.1/rsl_rl/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021-2025, ETH Zurich and NVIDIA CORPORATION
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Main module for the rsl_rl package."""
+
+__version__ = "2.1.1"
+__license__ = "BSD-3"
