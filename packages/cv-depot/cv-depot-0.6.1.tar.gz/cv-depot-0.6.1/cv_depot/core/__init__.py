5a94d14564a4ac84bd6be1dd858888771d9a0223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:03:46 +0000

    Add package cv-depot/0.6.1/cv-depot-0.6.1.tar.gz

---
+++ packages/cv-depot/cv-depot-0.6.1.tar.gz/cv-depot-0.6.1/cv_depot/core/__init__.py
@@ -0,0 +1,6 @@
+import cv_depot.core.enum  # noqa F401
+import cv_depot.core.channel  # noqa F401
+import cv_depot.core.color  # noqa F401
+import cv_depot.core.image  # noqa F401
+import cv_depot.core.tools  # noqa F401
+import cv_depot.core.viewer  # noqa F401
