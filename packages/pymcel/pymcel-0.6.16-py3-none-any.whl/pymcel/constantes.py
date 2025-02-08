33d03f014cd3f071f1687874c1602ebf119bb02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 14:14:07 +0000

    Add package pymcel/0.6.16/pymcel-0.6.16-py3-none-any.whl

---
+++ packages/pymcel/pymcel-0.6.16-py3-none-any.whl/pymcel/constantes.py
@@ -0,0 +1,26 @@
+#############################################################
+# CONSTANTES DE ASTROPY
+#############################################################
+lista = [
+    'G', 'N_A', 'R', 'Ryd', 'a0', 'alpha', 'atm', 
+    'b_wien', 'c', 'e', 'eps0', 'g0', 'h', 'hbar', 'k_B', 'm_e', 'm_n', 'm_p', 'mu0', 'muB', 
+    'sigma_T', 'sigma_sb', 'u', 'GM_earth', 'GM_jup', 'GM_sun', 'L_bol0', 'L_sun', 'M_earth', 'M_jup', 
+    'M_sun', 'R_earth', 'R_jup', 'R_sun', 'au', 'kpc', 'pc', 
+]
+exec('from astropy.constants import ' + ', '.join(lista))
+for constante in lista:
+    exec(f"{constante} = {constante}.value")
+
+#############################################################
+# OTRAS CONSTANTES
+#############################################################
+lista += ['año','día',]
+día = 24 * 60 * 60
+año = 365.25 * día
+dia = día
+yr = año
+
+#############################################################
+# CONSTANTES ACTUALIZADAS
+#############################################################
+mu_msun = 1.32712440041939403e+20 # m^3/s^2, obtenido de Horizons, DE441
