ff87c4f373ae96c88b6f9b1dd40a1fd70b16b175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:06 +0000

    Add package beamlit/0.0.54/beamlit-0.0.54-py3-none-any.whl

---
+++ packages/beamlit/beamlit-0.0.54-py3-none-any.whl/beamlit/api/accounts/update_account.py
@@ -0,0 +1,176 @@
+from http import HTTPStatus
+from typing import Any, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.account import Account
+from ...types import Response
+
+
+def _get_kwargs(
+    account_id: str,
+    *,
+    body: Account,
+) -> dict[str, Any]:
+    headers: dict[str, Any] = {}
+
+    _kwargs: dict[str, Any] = {
+        "method": "put",
+        "url": f"/accounts/{account_id}",
+    }
+
+    _body = body.to_dict()
+
+    _kwargs["json"] = _body
+    headers["Content-Type"] = "application/json"
+
+    _kwargs["headers"] = headers
+    return _kwargs
+
+
+def _parse_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Optional[Account]:
+    if response.status_code == 200:
+        response_200 = Account.from_dict(response.json())
+
+        return response_200
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Response[Account]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    account_id: str,
+    *,
+    client: AuthenticatedClient,
+    body: Account,
+) -> Response[Account]:
+    """Update account
+
+     Updates an account by name.
+
+    Args:
+        account_id (str):
+        body (Account): Account
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Account]
+    """
+
+    kwargs = _get_kwargs(
+        account_id=account_id,
+        body=body,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    account_id: str,
+    *,
+    client: AuthenticatedClient,
+    body: Account,
+) -> Optional[Account]:
+    """Update account
+
+     Updates an account by name.
+
+    Args:
+        account_id (str):
+        body (Account): Account
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Account
+    """
+
+    return sync_detailed(
+        account_id=account_id,
+        client=client,
+        body=body,
+    ).parsed
+
+
+async def asyncio_detailed(
+    account_id: str,
+    *,
+    client: AuthenticatedClient,
+    body: Account,
+) -> Response[Account]:
+    """Update account
+
+     Updates an account by name.
+
+    Args:
+        account_id (str):
+        body (Account): Account
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Account]
+    """
+
+    kwargs = _get_kwargs(
+        account_id=account_id,
+        body=body,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    account_id: str,
+    *,
+    client: AuthenticatedClient,
+    body: Account,
+) -> Optional[Account]:
+    """Update account
+
+     Updates an account by name.
+
+    Args:
+        account_id (str):
+        body (Account): Account
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Account
+    """
+
+    return (
+        await asyncio_detailed(
+            account_id=account_id,
+            client=client,
+            body=body,
+        )
+    ).parsed
