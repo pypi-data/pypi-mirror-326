9cd775d64338d4bf95ded458c714f7945749dda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:12 +0000

    Add package baseapp-backend/0.0.12/baseapp-backend-0.0.12.tar.gz

---
+++ packages/baseapp-backend/baseapp-backend-0.0.12.tar.gz/baseapp-backend-0.0.12/testproject/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+from baseapp_core.graphql import GraphQLView
+
+__all__ = [
+    "urlpatterns",
+]
+
+urlpatterns = [
+    path("graphql", GraphQLView.as_view(graphiql=True)),
+    path("admin/", admin.site.urls),
+]
