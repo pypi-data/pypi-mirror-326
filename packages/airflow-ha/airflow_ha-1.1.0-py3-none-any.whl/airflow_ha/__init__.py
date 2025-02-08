52517fdd5139d1f0c8a4b38dd9b490d40b8e8b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 14:14:08 +0000

    Add package airflow-ha/1.1.0/airflow_ha-1.1.0-py3-none-any.whl

---
+++ packages/airflow-ha/airflow_ha-1.1.0-py3-none-any.whl/airflow_ha/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "1.1.0"
+
+from .common import *
+from .utils import *
+
+try:
+    from .operator import *
+except ImportError:
+    # Airflow is not installed,
+    # don't raise an error for e.g.
+    # airflow-supervisor client
+    pass
