45b76d1edfcf62fa32a3436d30d1a91dd61c0d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:14:34 +0000

    Add package bobbie/0.1.7/bobbie-0.1.7-py3-none-any.whl

---
+++ packages/bobbie/bobbie-0.1.7-py3-none-any.whl/bobbie/__init__.py
@@ -0,0 +1,13 @@
+"""Flexible, easy python project configuration"""
+
+from __future__ import annotations
+
+__version__ = '0.1.7'
+
+__author__: str = 'Corey Rayburn Yung'
+
+
+from .base import Settings
+
+__all__: list[str] = [
+    "Settings"]
