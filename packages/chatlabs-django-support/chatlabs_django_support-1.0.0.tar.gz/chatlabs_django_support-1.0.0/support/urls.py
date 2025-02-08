4dc4dba8c423f700a39d028b95d32bde646765d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:03:54 +0000

    Add package chatlabs-django-support/1.0.0/chatlabs_django_support-1.0.0.tar.gz

---
+++ packages/chatlabs-django-support/chatlabs_django_support-1.0.0.tar.gz/chatlabs_django_support-1.0.0/support/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('manager/', views.Support.as_view(), name='manager'),
+    path('tickets/', views.TicketList.as_view(), name='tickets'),
+    path('tickets/<int:ticket_id>/', views.Ticket.as_view(), name='ticket'),
+    path(
+        'tickets/<int:ticket_id>/messages/',
+        views.MessageList.as_view(),
+        name='messages',
+    ),
+]
