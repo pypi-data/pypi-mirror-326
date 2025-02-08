795b41ca83da02d08be3abed6ce47b2aaf2dfa99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:17:45 +0000

    Add package xapi-guard-middleware/1.0.6/xapi_guard_middleware-1.0.6.tar.gz

---
+++ packages/xapi-guard-middleware/xapi_guard_middleware-1.0.6.tar.gz/xapi_guard_middleware-1.0.6/xapi_guard_middleware/__init__.py
@@ -0,0 +1,4 @@
+from .middleware import XAPIGuardMiddleware
+
+__version__ = "1.0.3"
+__all__ = ["XAPIGuardMiddleware"]
---
+++ packages/xapi-guard-middleware/xapi_guard_middleware-1.0.6.tar.gz/xapi_guard_middleware-1.0.6/xapi_guard_middleware/error_codes.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class APIErrorCode(Enum):
+    """Enumeration of API error codes."""
+
+    INVALID_API_KEY = "INVALID_API_KEY"
+    """Error code for invalid API key."""
