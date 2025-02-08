396c5f5f5138d5bab4dee2235cda58e1d36772ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 14:03:56 +0000

    Add package disgrasya/10.40.8/disgrasya-10.40.8.tar.gz

---
+++ packages/disgrasya/disgrasya-10.40.8.tar.gz/disgrasya-10.40.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='disgrasya',
+    version='10.40.8',
+    description='A utility for checking credit cards through multiple gateways using multi-threading and proxies.',
+    author='Jaehwan0',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'disgrasya=disgrasya.main:main',
+        ],
+    },
+)
