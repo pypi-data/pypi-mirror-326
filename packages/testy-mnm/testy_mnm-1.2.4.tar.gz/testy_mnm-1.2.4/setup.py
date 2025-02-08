86d47d4e704c0785e36f3af376c0318fb442126f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:03:55 +0000

    Add package testy-mnm/1.2.4/testy_mnm-1.2.4.tar.gz

---
+++ packages/testy-mnm/testy_mnm-1.2.4.tar.gz/testy_mnm-1.2.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages, Extension
+from distutils.command.build import build
+import os
+import shutil
+import sys
+import numpy as np
+
+def find_all_c_files(directory):
+    c_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.c'):
+                dir = os.path.join(root, file)
+                dir = dir.replace('\\', '/')
+                c_files.append(dir)
+
+    return c_files
+
+testy_c_module = Extension(
+    'testy_mnm.core.testy_c_module',
+    sources=find_all_c_files('testy_mnm/core'),
+    include_dirs=[
+        "testy_mnm/core/src",
+        "testy_mnm/core/src/src_of_src",
+        np.get_include()
+    ],
+)
+
+setup(
+    name='testy_mnm',
+    version='1.2.4',
+    packages=find_packages(),
+    ext_modules=[testy_c_module],
+)
