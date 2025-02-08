433f45293d3dbb7bb618756e1d75a99f2d1e3c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:05:20 +0000

    Add package ops.manifest/1.4.0/ops_manifest-1.4.0.tar.gz

---
+++ packages/ops.manifest/ops_manifest-1.4.0.tar.gz/ops_manifest-1.4.0/ops/manifests/__init__.py
@@ -0,0 +1,27 @@
+from .collector import Collector
+from .exceptions import ManifestClientError
+from .manifest import HashableResource, Manifests
+from .manipulations import (
+    Addition,
+    ConfigRegistry,
+    CreateNamespace,
+    ManifestLabel,
+    Patch,
+    SubtractEq,
+    update_tolerations,
+)
+
+__all__ = [
+    "Addition",
+    "Collector",
+    "ConfigRegistry",
+    "CreateNamespace",
+    "HashableResource",
+    "ManifestLabel",
+    "Manifests",
+    "Patch",
+    "update_tolerations",
+    "SubtractEq",
+    "ManifestClientError",
+    "update_tolerations",
+]
