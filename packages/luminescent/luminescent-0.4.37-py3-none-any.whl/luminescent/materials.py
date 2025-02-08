3682422b6d5158ff127b66a8bfeddbfedfa5a932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:25:52 +0000

    Add package luminescent/0.4.37/luminescent-0.4.37-py3-none-any.whl

---
+++ packages/luminescent/luminescent-0.4.37-py3-none-any.whl/luminescent/materials.py
@@ -0,0 +1,27 @@
+
+MATKEYS = {
+    "si": "cSi",
+    "Si": "cSi",
+
+    "sio2": "SiO2",
+    "sin": "SiN",
+    "ge": "Ge",
+
+}
+
+
+def matname(k):
+    if k in MATKEYS:
+        return MATKEYS[k]
+    return k.capitalize()
+
+
+MATERIALS = {
+    "cSi": {
+        'epsilon': 3.48**2},
+    "SiO2": {'epsilon': 1.44**2},
+    "SiN": {'epsilon': 2.0**2},
+    "Ge": {'epsilon': 4.0**2},
+    "Si": {'epsilon': 3.48**2},
+    'PEC': {'epsilon': 10000},
+}
---
+++ packages/luminescent/luminescent-0.4.37-py3-none-any.whl/luminescent/runs_utils.py
@@ -0,0 +1,73 @@
+# import dill
+from pprint import pprint
+import os
+import json
+import numpy as np
+from .pic.inverse_design import *
+from .pic.sparams import *
+from .utils import *
+from .layers import *
+from .constants import *
+
+
+def lastrun(name="", wd=os.path.join(os.getcwd(), "runs"), study="",  **kwargs):
+    if name:
+        return os.path.join(wd, name)
+    l = [os.path.join(wd, x) for x in os.listdir(wd)]
+    l = [x for x in l if os.path.isfile(os.path.join(x, "problem.json"))]
+    l = sorted(l, key=lambda x: os.path.getmtime(x), reverse=True)
+    if study:
+        for x in l:
+            try:
+                if json.loads(open(os.path.join(x, "problem.json"), "rb").read())["study"] == study:
+                    return x
+            except:
+                pass
+    return l[0]
+
+
+def load_problem(path):
+    path = os.path.abspath(path)
+    print(f"loading problem from {path}")
+    return json.loads(open(os.path.join(path, "problem.json"), "rb").read())
+
+
+def make_simulation_movie(path, framerate=30, **kwargs):
+    dir = os.path.join(path, "temp", 'fields')
+    umax = gmax = 0
+    fns = sorted(os.listdir(dir), key=lambda x: float(x[0:-4]))
+    for fn in fns:
+        a = np.load(os.path.join(dir, fn))
+        v = np.max(np.abs(a))
+        if umax < v:
+            umax = v
+    frame = cv2.imread(os.path.join(f, imgs[0]))
+    height, width, layers = frame.shape
+
+    video = cv2.VideoWriter(os.path.join(
+        path, "simulation_video.mp4"), 0x7634706d, framerate, (width, height))
+
+    for img in imgs:
+        video.write(cv2.imread(os.path.join(f, img)))
+
+    cv2.destroyAllWindows()
+    video.release()
+
+
+def make_training_movie(framerate=2, **kwargs):
+    1
+    # path = lastrun(**kwargs)
+
+    # f = os.path.join(path, "checkpoints")
+    # ckpts = sorted(os.listdir(f))
+    # frame = cv2.imread(os.path.join(f, ckpts[0], "run_1.png"))
+    # height, width, layers = frame.shape
+
+    # video = cv2.VideoWriter(os.path.join(
+    #     path, "training_video.mp4"), 0x7634706d, framerate, (width, height))
+
+    # for ckpt in ckpts:
+    #     video.write(cv2.imread(os.path.join(f, ckpt, "run_1.png")))
+
+    # cv2.destroyAllWindows()
+    # video.release()
