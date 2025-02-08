250b172a220ce282bab7b814882b7100876b5244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:03:45 +0000

    Add package mcfinder/1.0.0/mcfinder-1.0.0-py3-none-any.whl

---
+++ packages/mcfinder/mcfinder-1.0.0-py3-none-any.whl/mcfinder/__init__.py
@@ -0,0 +1,7 @@
+# mcfinder/__init__.py
+
+# Ici, tu peux définir des variables, des fonctions ou des classes accessibles globalement dans ton package
+
+from .pinger import main  # Permet d'accéder directement à la fonction main depuis mcfinder.main
+
+# Autres initialisations ou imports peuvent être ajoutés ici si nécessaire
