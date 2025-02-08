e7be591616e146579b1ef29411c2a6d4ca42502e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:03:39 +0000

    Add package sleipnirgroup-jormungandr/0.0.1.dev354/sleipnirgroup_jormungandr-0.0.1.dev354.tar.gz

---
+++ packages/sleipnirgroup-jormungandr/sleipnirgroup_jormungandr-0.0.1.dev354.tar.gz/sleipnirgroup_jormungandr-0.0.1.dev354/include/sleipnir/autodiff/AdjointExpressionGraph.hpp
@@ -0,0 +1,218 @@
+// Copyright (c) Sleipnir contributors
+
+#pragma once
+
+#include <ranges>
+#include <utility>
+
+#include <Eigen/SparseCore>
+
+#include "sleipnir/autodiff/Variable.hpp"
+#include "sleipnir/autodiff/VariableMatrix.hpp"
+#include "sleipnir/util/small_vector.hpp"
+
+namespace sleipnir::detail {
+
+/**
+ * This class is an adaptor type that performs value updates of an expression's
+ * adjoint graph.
+ */
+class AdjointExpressionGraph {
+ public:
+  /**
+   * Generates the adjoint graph for the given expression.
+   *
+   * @param root The root node of the expression.
+   */
+  explicit AdjointExpressionGraph(const Variable& root) {
+    // If the root type is a constant, Update() is a no-op, so there's no work
+    // to do
+    if (root.expr == nullptr || root.Type() == ExpressionType::kConstant) {
+      return;
+    }
+
+    // Stack of nodes to explore
+    small_vector<Expression*> stack;
+
+    // Enumerate incoming edges for each node via depth-first search
+    stack.emplace_back(root.expr.Get());
+    while (!stack.empty()) {
+      auto node = stack.back();
+      stack.pop_back();
+
+      for (auto& arg : node->args) {
+        // If the node hasn't been explored yet, add it to the stack
+        if (arg != nullptr && ++arg->incomingEdges == 1) {
+          stack.push_back(arg.Get());
+        }
+      }
+    }
+
+    // Generate topological sort of graph from parent to child.
+    //
+    // A node is only added to the stack after all its incoming edges have been
+    // traversed. Expression::incomingEdges is a decrementing counter for
+    // tracking this.
+    //
+    // https://en.wikipedia.org/wiki/Topological_sorting
+    stack.emplace_back(root.expr.Get());
+    while (!stack.empty()) {
+      auto node = stack.back();
+      stack.pop_back();
+
+      m_colList.emplace_back(node->col);
+      m_adjointList.emplace_back(node);
+      if (node->args[0] != nullptr) {
+        // Constants (expressions with no arguments) are skipped because they
+        // don't need to be updated
+        m_valueList.emplace_back(node);
+      }
+
+      for (auto& arg : node->args) {
+        // If we traversed all this node's incoming edges, add it to the stack
+        if (arg != nullptr && --arg->incomingEdges == 0) {
+          stack.push_back(arg.Get());
+        }
+      }
+    }
+  }
+
+  /**
+   * Update the values of all nodes in this adjoint graph based on the values of
+   * their dependent nodes.
+   */
+  void Update() {
+    // Traverse the BFS list backward from child to parent and update the value
+    // of each node.
+    for (auto& node : m_valueList | std::views::reverse) {
+      auto& lhs = node->args[0];
+      auto& rhs = node->args[1];
+
+      if (lhs != nullptr) {
+        if (rhs != nullptr) {
+          node->value = node->Value(lhs->value, rhs->value);
+        } else {
+          node->value = node->Value(lhs->value, 0.0);
+        }
+      }
+    }
+  }
+
+  /**
+   * Returns the variable's gradient tree.
+   *
+   * This function lazily allocates variables, so elements of the returned
+   * VariableMatrix will be empty if the corresponding element of wrt had no
+   * adjoint. Ensure Variable::expr isn't nullptr before calling member
+   * functions.
+   *
+   * @param wrt Variables with respect to which to compute the gradient.
+   */
+  VariableMatrix GenerateGradientTree(const VariableMatrix& wrt) const {
+    // Read docs/algorithms.md#Reverse_accumulation_automatic_differentiation
+    // for background on reverse accumulation automatic differentiation.
+
+    if (m_adjointList.empty()) {
+      return VariableMatrix{VariableMatrix::empty, wrt.Rows(), 1};
+    }
+
+    // Set root node's adjoint to 1 since df/df is 1
+    m_adjointList[0]->adjointExpr = MakeExpressionPtr<ConstExpression>(1.0);
+
+    // df/dx = (df/dy)(dy/dx). The adjoint of x is equal to the adjoint of y
+    // multiplied by dy/dx. If there are multiple "paths" from the root node to
+    // variable; the variable's adjoint is the sum of each path's adjoint
+    // contribution.
+    for (auto& node : m_adjointList) {
+      auto& lhs = node->args[0];
+      auto& rhs = node->args[1];
+
+      if (lhs != nullptr) {
+        lhs->adjointExpr =
+            lhs->adjointExpr + node->GradientLhs(lhs, rhs, node->adjointExpr);
+        if (rhs != nullptr) {
+          rhs->adjointExpr =
+              rhs->adjointExpr + node->GradientRhs(lhs, rhs, node->adjointExpr);
+        }
+      }
+    }
+
+    // Move gradient tree to return value
+    VariableMatrix grad{VariableMatrix::empty, wrt.Rows(), 1};
+    for (int row = 0; row < grad.Rows(); ++row) {
+      grad(row) = Variable{std::move(wrt(row).expr->adjointExpr)};
+    }
+
+    // Unlink adjoints to avoid circular references between them and their
+    // parent expressions. This ensures all expressions are returned to the free
+    // list.
+    for (auto& node : m_adjointList) {
+      node->adjointExpr = nullptr;
+    }
+
+    return grad;
+  }
+
+  /**
+   * Updates the adjoints in the expression graph (computes the gradient) then
+   * appends the adjoints of wrt to the sparse matrix triplets.
+   *
+   * @param triplets The sparse matrix triplets.
+   * @param row The row of wrt.
+   */
+  void AppendAdjointTriplets(small_vector<Eigen::Triplet<double>>& triplets,
+                             int row) const {
+    // Read docs/algorithms.md#Reverse_accumulation_automatic_differentiation
+    // for background on reverse accumulation automatic differentiation.
+
+    if (m_adjointList.empty()) {
+      return;
+    }
+
+    // Set root node's adjoint to 1 since df/df is 1
+    m_adjointList[0]->adjoint = 1.0;
+
+    // df/dx = (df/dy)(dy/dx). The adjoint of x is equal to the adjoint of y
+    // multiplied by dy/dx. If there are multiple "paths" from the root node to
+    // variable; the variable's adjoint is the sum of each path's adjoint
+    // contribution.
+    for (const auto& [node, col] : std::views::zip(m_adjointList, m_colList)) {
+      auto& lhs = node->args[0];
+      auto& rhs = node->args[1];
+
+      if (lhs != nullptr) {
+        if (rhs != nullptr) {
+          lhs->adjoint +=
+              node->GradientValueLhs(lhs->value, rhs->value, node->adjoint);
+          rhs->adjoint +=
+              node->GradientValueRhs(lhs->value, rhs->value, node->adjoint);
+        } else {
+          lhs->adjoint +=
+              node->GradientValueLhs(lhs->value, 0.0, node->adjoint);
+        }
+      }
+
+      // Append adjoints of wrt to sparse matrix triplets
+      if (col != -1 && node->adjoint != 0.0) {
+        triplets.emplace_back(row, col, node->adjoint);
+      }
+    }
+
+    // Zero adjoints for next run
+    for (auto& node : m_adjointList) {
+      node->adjoint = 0.0;
+    }
+  }
+
+ private:
+  // List that maps nodes to their respective column
+  small_vector<int> m_colList;
+
+  // List for updating adjoints
+  small_vector<Expression*> m_adjointList;
+
+  // List for updating values
+  small_vector<Expression*> m_valueList;
+};
+
+}  // namespace sleipnir::detail
---
+++ packages/sleipnirgroup-jormungandr/sleipnirgroup_jormungandr-0.0.1.dev354.tar.gz/sleipnirgroup_jormungandr-0.0.1.dev354/src/util/PrintDiagnostics.hpp
@@ -0,0 +1,210 @@
+// Copyright (c) Sleipnir contributors
+
+#pragma once
+
+#include <stdint.h>
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <ranges>
+#include <string>
+#include <utility>
+
+#include "sleipnir/util/Print.hpp"
+#include "sleipnir/util/SetupProfiler.hpp"
+#include "sleipnir/util/SolveProfiler.hpp"
+#include "sleipnir/util/small_vector.hpp"
+#include "util/ToMs.hpp"
+
+namespace sleipnir {
+
+/**
+ * Iteration mode.
+ */
+enum class IterationMode : uint8_t {
+  /// Normal iteration.
+  kNormal,
+  /// Second-order correction iteration.
+  kSecondOrderCorrection
+};
+
+/**
+ * Prints diagnostics for the current iteration.
+ *
+ * @param iterations Number of iterations.
+ * @param mode Which mode the iteration was in.
+ * @param time The iteration duration.
+ * @param error The error.
+ * @param cost The cost.
+ * @param infeasibility The infeasibility.
+ * @param δ The Hessian regularization factor.
+ * @param primal_α The primal step size.
+ * @param dual_α The dual step size.
+ */
+template <typename Rep, typename Period = std::ratio<1>>
+void PrintIterationDiagnostics(int iterations, IterationMode mode,
+                               const std::chrono::duration<Rep, Period>& time,
+                               double error, double cost, double infeasibility,
+                               double δ, double primal_α, double dual_α) {
+  if (iterations % 20 == 0) {
+    if (iterations == 0) {
+      sleipnir::println(
+          "┏{:━^6}┯{:━^6}┯{:━^11}┯{:━^14}┯{:━^14}┯{:━^15}┯{:━^7}┯{:━^10}┯"
+          "{:━^10}┯{:━^4}┓",
+          "", "", "", "", "", "", "", "", "", "");
+    } else {
+      sleipnir::println(
+          "┢{:━^6}┯{:━^6}┯{:━^11}┯{:━^14}┯{:━^14}┯{:━^15}┯{:━^7}┯{:━^10}┯"
+          "{:━^10}┯{:━^4}┪",
+          "", "", "", "", "", "", "", "", "", "");
+    }
+    sleipnir::println(
+        "┃ {:^4} │ {:^4} │ {:^9} │ {:^12} │ {:^12} │ {:^13} │ {:^5} │ {:^8} │ "
+        "{:^8} │ {:^2} ┃",
+        "iter", "mode", "time (ms)", "error", "cost", "infeasibility", "reg",
+        "primal α", "dual α", "↩");
+    sleipnir::println(
+        "┡{:━^6}┷{:━^6}┷{:━^11}┷{:━^14}┷{:━^14}┷{:━^15}┷{:━^7}┷{:━^10}┷{:━^10}┷"
+        "{:━^4}┩",
+        "", "", "", "", "", "", "", "", "", "");
+  }
+
+  constexpr const char* kIterationModes[] = {"norm", "SOC"};
+  sleipnir::print("│ {:4}   {:4}   {:9.3f}   {:12e}   {:12e}   {:13e}   ",
+                  iterations, kIterationModes[std::to_underlying(mode)],
+                  ToMs(time), error, cost, infeasibility);
+
+  // Print regularization
+  if (δ == 0.0) {
+    sleipnir::print(" 0   ");
+  } else {
+    int exponent = std::log10(δ);
+
+    if (exponent == 0) {
+      sleipnir::print(" 1   ");
+    } else if (exponent == 1) {
+      sleipnir::print("10   ");
+    } else {
+      // Gather regularization exponent digits
+      int n = std::abs(exponent);
+      small_vector<int> digits;
+      do {
+        digits.emplace_back(n % 10);
+        n /= 10;
+      } while (n > 0);
+
+      std::string reg = "10";
+
+      // Append regularization exponent
+      if (exponent < 0) {
+        reg += "⁻";
+      }
+      constexpr const char* strs[] = {"⁰", "¹", "²", "³", "⁴",
+                                      "⁵", "⁶", "⁷", "⁸", "⁹"};
+      for (const auto& digit : digits | std::views::reverse) {
+        reg += strs[digit];
+      }
+
+      sleipnir::print("{:<5}", reg);
+    }
+  }
+
+  // Print step sizes and number of backtracks
+  sleipnir::println("   {:.2e}   {:.2e}   {:2d} │", primal_α, dual_α,
+                    static_cast<int>(-std::log2(primal_α)));
+}
+
+/**
+ * Renders histogram of the given normalized value.
+ *
+ * @tparam Width Width of the histogram in characters.
+ * @param value Normalized value from 0 to 1.
+ */
+template <int Width>
+  requires(Width > 0)
+inline std::string Histogram(double value) {
+  value = std::clamp(value, 0.0, 1.0);
+
+  double ipart;
+  int fpart = static_cast<int>(std::modf(value * Width, &ipart) * 8);
+
+  constexpr const char* strs[] = {" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"};
+  std::string hist;
+
+  int index = 0;
+  while (index < ipart) {
+    hist += strs[8];
+    ++index;
+  }
+  if (fpart > 0) {
+    hist += strs[fpart];
+    ++index;
+  }
+  while (index < Width) {
+    hist += strs[0];
+    ++index;
+  }
+
+  return hist;
+}
+
+/**
+ * Prints final diagnostics.
+ *
+ * @param iterations Number of iterations.
+ * @param setupProfilers Setup profilers.
+ * @param solveProfilers Solve profilers.
+ */
+inline void PrintFinalDiagnostics(
+    int iterations, const small_vector<SetupProfiler>& setupProfilers,
+    const small_vector<SolveProfiler>& solveProfilers) {
+  // Print bottom of iteration diagnostics table
+  sleipnir::println("└{:─^106}┘", "");
+
+  // Print total time
+  auto setupDuration = ToMs(setupProfilers[0].Duration());
+  auto solveDuration = ToMs(solveProfilers[0].TotalDuration());
+  sleipnir::println("\nTime: {:.3f} ms", setupDuration + solveDuration);
+  sleipnir::println("  ↳ setup: {:.3f} ms", setupDuration);
+  sleipnir::println("  ↳ solve: {:.3f} ms ({} iterations)", solveDuration,
+                    iterations);
+
+  // Print setup diagnostics
+  sleipnir::println("\n┏{:━^23}┯{:━^20}┯{:━^12}┓", "", "", "");
+  sleipnir::println("┃ {:^21} │ {:^18} │ {:^10} ┃", "trace", "percent",
+                    "total (ms)");
+  sleipnir::println("┡{:━^23}┷{:━^20}┷{:━^12}┩", "", "", "");
+
+  for (auto& profiler : setupProfilers) {
+    double norm =
+        ToMs(profiler.Duration()) / ToMs(setupProfilers[0].Duration());
+    sleipnir::println("│ {:<21}   {:>6.2f}%▕{}▏   {:>10.3f} │", profiler.name,
+                      norm * 100.0, Histogram<9>(norm),
+                      ToMs(profiler.Duration()));
+  }
+
+  sleipnir::println("└{:─^57}┘", "");
+
+  // Print solve diagnostics
+  sleipnir::println("┏{:━^23}┯{:━^20}┯{:━^12}┯{:━^11}┯{:━^6}┓", "", "", "", "",
+                    "");
+  sleipnir::println("┃ {:^21} │ {:^18} │ {:^10} │ {:^9} │ {:^4} ┃", "trace",
+                    "percent", "total (ms)", "each (ms)", "runs");
+  sleipnir::println("┡{:━^23}┷{:━^20}┷{:━^12}┷{:━^11}┷{:━^6}┩", "", "", "", "",
+                    "");
+
+  for (auto& profiler : solveProfilers) {
+    double norm = ToMs(profiler.TotalDuration()) /
+                  ToMs(solveProfilers[0].TotalDuration());
+    sleipnir::println(
+        "│ {:<21}   {:>6.2f}%▕{}▏   {:>10.3f}   {:>9.3f}   {:>4} │",
+        profiler.name, norm * 100.0, Histogram<9>(norm),
+        ToMs(profiler.TotalDuration()), ToMs(profiler.AverageDuration()),
+        profiler.NumSolves());
+  }
+
+  sleipnir::println("└{:─^76}┘\n", "");
+}
+
+}  // namespace sleipnir
