03c8058b68da33f37a8ac209e9a62788c656045e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:05 +0000

    Add package dreamify/0.20.2/dreamify-0.20.2-py3-none-any.whl

---
+++ packages/dreamify/dreamify-0.20.2-py3-none-any.whl/dreamify/utils/configure.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+from dreamify.lib.image_to_video_converter import ImageToVideoConverter
+
+# , field
+
+# @dataclass
+# class Config:
+#     feature_extractor: object = None
+#     layer_settings: object = None
+#     original_shape: object = None
+#     enable_framing: bool = False
+#     frames_for_vid: list = field(default_factory=list)
+#     max_frames_to_sample: int = 0
+#     curr_frame_idx: int = 0
+
+
+@dataclass
+class Config:
+    feature_extractor: object = None
+    layer_settings: object = None
+    original_shape: object = None
+    enable_framing: bool = False
+    max_frames_to_sample: int = 0
+    framer: ImageToVideoConverter = None
+
+    def __post_init__(self):
+        if self.framer is None:
+            self.framer = ImageToVideoConverter(
+                max_frames_to_sample=self.max_frames_to_sample
+            )
