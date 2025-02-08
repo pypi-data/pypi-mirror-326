f4198877922977d414cf901935f496db971ab4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:05:39 +0000

    Add package nillion-signatures/0.1.0/nillion_signatures-0.1.0.tar.gz

---
+++ packages/nillion-signatures/nillion_signatures-0.1.0.tar.gz/nillion_signatures-0.1.0/README.md
@@ -0,0 +1,3 @@
+# Crypto Signing Mock
+
+A mock library for cryptographic signing operations using ECDSA and EdDSA. 
---
+++ packages/nillion-signatures/nillion_signatures-0.1.0.tar.gz/nillion_signatures-0.1.0/nillion_signatures/__init__.py
@@ -0,0 +1,5 @@
+from .key_gen import key_gen
+from .sign import sign
+
+__version__ = "0.1.1"
+__all__ = ['key_gen', 'sign'] 
