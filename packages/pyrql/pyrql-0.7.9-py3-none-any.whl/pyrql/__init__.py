0c74a68a2cc2dd85e0c76344922466b4c4d24b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:25:53 +0000

    Add package pyrql/0.7.9/pyrql-0.7.9-py3-none-any.whl

---
+++ packages/pyrql/pyrql-0.7.9-py3-none-any.whl/pyrql/__init__.py
@@ -0,0 +1,18 @@
+from pyrql.exceptions import RQLError
+from pyrql.exceptions import RQLQueryError
+from pyrql.exceptions import RQLSyntaxError
+from pyrql.parser import Parser
+from pyrql.query import Query
+from pyrql.unparser import Unparser
+
+__title__ = "pyrql"
+__version__ = "0.7.9"
+__author__ = "Pedro Werneck"
+__license__ = "MIT"
+
+
+parse = Parser().parse
+
+unparse = Unparser().unparse
+
+__all__ = ["parse", "unparse", "Query", "RQLError", "RQLQueryError", "RQLSyntaxError"]
