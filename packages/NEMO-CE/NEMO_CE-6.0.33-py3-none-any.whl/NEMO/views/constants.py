fa242ba6c412e07fa5d848129a141c68f0f9adb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:05:50 +0000

    Add package NEMO-CE/6.0.33/NEMO_CE-6.0.33-py3-none-any.whl

---
+++ packages/NEMO-CE/NEMO_CE-6.0.33-py3-none-any.whl/NEMO/views/constants.py
@@ -0,0 +1,16 @@
+# Maximum length for additional information of reservations
+ADDITIONAL_INFORMATION_MAXIMUM_LENGTH = 3000
+
+# Maximum length for feedback emails
+FEEDBACK_MAXIMUM_LENGTH = 5000
+
+SYSTEM_USER_DISPLAY = "System"
+
+# Maximum length for Char fields to work with every db type
+CHAR_FIELD_MAXIMUM_LENGTH = 255
+
+# Name of the parameter to indicate which view to redirect to
+NEXT_PARAMETER_NAME = "next"
+
+# Name of the media folder under which only staff can see files
+MEDIA_PROTECTED = "protected"
