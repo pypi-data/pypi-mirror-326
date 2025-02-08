7c9abb184047387637e45ef59e301a350219befb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 05:04:23 +0000

    Add package stream-translator-gpt/2025.2.8/stream_translator_gpt-2025.2.8.tar.gz

---
+++ packages/stream-translator-gpt/stream_translator_gpt-2025.2.8.tar.gz/stream_translator_gpt-2025.2.8/stream_translator_gpt/audio_getter.py
@@ -0,0 +1,143 @@
+import os
+import queue
+import signal
+import subprocess
+import sys
+import threading
+
+import ffmpeg
+import numpy as np
+
+from .common import SAMPLE_RATE, LoopWorkerBase
+
+
+def _transport(ytdlp_proc, ffmpeg_proc):
+    while (ytdlp_proc.poll() is None) and (ffmpeg_proc.poll() is None):
+        try:
+            chunk = ytdlp_proc.stdout.read(1024)
+            ffmpeg_proc.stdin.write(chunk)
+        except (BrokenPipeError, OSError):
+            pass
+    ytdlp_proc.kill()
+    ffmpeg_proc.kill()
+
+
+def _open_stream(url: str, format: str, cookies: str, proxy: str):
+    cmd = ['yt-dlp', url, '-f', format, '-o', '-', '-q']
+    if cookies:
+        cmd.extend(['--cookies', cookies])
+    if proxy:
+        cmd.extend(['--proxy', proxy])
+    ytdlp_process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+    try:
+        ffmpeg_process = (ffmpeg.input('pipe:', loglevel='panic').output('pipe:',
+                                                                         format='s16le',
+                                                                         acodec='pcm_s16le',
+                                                                         ac=1,
+                                                                         ar=SAMPLE_RATE).run_async(pipe_stdin=True,
+                                                                                                   pipe_stdout=True))
+    except ffmpeg.Error as e:
+        raise RuntimeError(f'Failed to load audio: {e.stderr.decode()}') from e
+
+    thread = threading.Thread(target=_transport, args=(ytdlp_process, ffmpeg_process))
+    thread.start()
+    return ffmpeg_process, ytdlp_process
+
+
+class StreamAudioGetter(LoopWorkerBase):
+
+    def __init__(self, url: str, format: str, cookies: str, proxy: str, frame_duration: float) -> None:
+        self._cleanup_ytdlp_cache()
+
+        print('Opening stream: {}'.format(url))
+        self.ffmpeg_process, self.ytdlp_process = _open_stream(url, format, cookies, proxy)
+        self.byte_size = round(frame_duration * SAMPLE_RATE *
+                               2)  # Factor 2 comes from reading the int16 stream as bytes
+        signal.signal(signal.SIGINT, self._exit_handler)
+
+    def __del__(self):
+        self._cleanup_ytdlp_cache()
+
+    def _exit_handler(self, signum, frame):
+        self.ffmpeg_process.kill()
+        if self.ytdlp_process:
+            self.ytdlp_process.kill()
+        sys.exit(0)
+
+    def _cleanup_ytdlp_cache(self):
+        for file in os.listdir('./'):
+            if file.startswith('--Frag'):
+                os.remove(file)
+
+    def loop(self, output_queue: queue.SimpleQueue[np.array]):
+        while self.ffmpeg_process.poll() is None:
+            in_bytes = self.ffmpeg_process.stdout.read(self.byte_size)
+            if not in_bytes:
+                break
+            if len(in_bytes) != self.byte_size:
+                continue
+            audio = np.frombuffer(in_bytes, np.int16).flatten().astype(np.float32) / 32768.0
+            output_queue.put(audio)
+
+        self.ffmpeg_process.kill()
+        if self.ytdlp_process:
+            self.ytdlp_process.kill()
+
+
+class LocalFileAudioGetter(LoopWorkerBase):
+
+    def __init__(self, file_path: str, frame_duration: float) -> None:
+        print('Opening local file: {}'.format(file_path))
+        try:
+            self.ffmpeg_process = (ffmpeg.input(file_path,
+                                                loglevel='panic').output('pipe:',
+                                                                         format='s16le',
+                                                                         acodec='pcm_s16le',
+                                                                         ac=1,
+                                                                         ar=SAMPLE_RATE).run_async(pipe_stdin=True,
+                                                                                                   pipe_stdout=True))
+        except ffmpeg.Error as e:
+            raise RuntimeError(f'Failed to load audio: {e.stderr.decode()}') from e
+        self.byte_size = round(frame_duration * SAMPLE_RATE *
+                               2)  # Factor 2 comes from reading the int16 stream as bytes
+        signal.signal(signal.SIGINT, self._exit_handler)
+
+    def _exit_handler(self, signum, frame):
+        self.ffmpeg_process.kill()
+        sys.exit(0)
+
+    def loop(self, output_queue: queue.SimpleQueue[np.array]):
+        while self.ffmpeg_process.poll() is None:
+            in_bytes = self.ffmpeg_process.stdout.read(self.byte_size)
+            if not in_bytes:
+                break
+            if len(in_bytes) != self.byte_size:
+                continue
+            audio = np.frombuffer(in_bytes, np.int16).flatten().astype(np.float32) / 32768.0
+            output_queue.put(audio)
+
+        self.ffmpeg_process.kill()
+
+
+class DeviceAudioGetter(LoopWorkerBase):
+
+    def __init__(self, device_index: int, frame_duration: float, recording_interval: float) -> None:
+        import sounddevice as sd
+        if device_index:
+            sd.default.device[0] = device_index
+        sd.default.dtype[0] = np.float32
+        self.frame_duration = frame_duration
+        self.recording_frame_num = max(1, round(recording_interval / frame_duration))
+        print('Recording device: {}'.format(sd.query_devices(sd.default.device[0])['name']))
+
+    def loop(self, output_queue: queue.SimpleQueue[np.array]):
+        import sounddevice as sd
+        while True:
+            audio = sd.rec(frames=round(SAMPLE_RATE * self.frame_duration * self.recording_frame_num),
+                           samplerate=SAMPLE_RATE,
+                           channels=1,
+                           blocking=True).flatten()
+            split_audios = np.array_split(audio, self.recording_frame_num)
+            for split_audio in split_audios:
+                output_queue.put(split_audio)
---
+++ packages/stream-translator-gpt/stream_translator_gpt-2025.2.8.tar.gz/stream_translator_gpt-2025.2.8/stream_translator_gpt/result_exporter.py
@@ -0,0 +1,71 @@
+import os
+import queue
+import requests
+
+from .common import TranslationTask, LoopWorkerBase, sec2str
+
+
+def _send_to_cqhttp(url: str, token: str, proxies: dict, text: str):
+    headers = {'Authorization': 'Bearer {}'.format(token)} if token else None
+    data = {'message': text}
+    try:
+        requests.post(url, headers=headers, data=data, timeout=10, proxies=proxies)
+    except Exception as e:
+        print(e)
+
+
+def _send_to_discord(webhook_url: str, proxies: dict, text: str):
+    for sub_text in text.split('\n'):
+        data = {'content': sub_text}
+        try:
+            requests.post(webhook_url, json=data, timeout=10, proxies=proxies)
+        except Exception as e:
+            print(e)
+
+
+def _send_to_telegram(token: str, chat_id: int, proxies: dict, text: str):
+    url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(token, chat_id, text)
+    try:
+        requests.post(url, timeout=10, proxies=proxies)
+    except Exception as e:
+        print(e)
+
+
+def _output_to_file(file_path: str, text: str):
+    with open(file_path, 'a', encoding='utf-8') as f:
+        f.write(text + '\n\n')
+
+
+class ResultExporter(LoopWorkerBase):
+
+    def __init__(self, output_file_path: str) -> None:
+        if output_file_path:
+            if os.path.exists(output_file_path):
+                os.remove(output_file_path)
+
+    def loop(self, input_queue: queue.SimpleQueue[TranslationTask], output_whisper_result: bool,
+             output_timestamps: bool, proxy: str, output_file_path: str, cqhttp_url: str, cqhttp_token: str,
+             discord_webhook_url: str, telegram_token: str, telegram_chat_id: int):
+        proxies = {"http": proxy, "https": proxy} if proxy else None
+        while True:
+            task = input_queue.get()
+            timestamp_text = '{} --> {}'.format(sec2str(task.time_range[0]), sec2str(task.time_range[1]))
+            text_to_send = (task.transcribed_text + '\n') if output_whisper_result else ''
+            if output_timestamps:
+                text_to_send = timestamp_text + '\n' + text_to_send
+            if task.translated_text:
+                text_to_print = task.translated_text
+                if output_timestamps:
+                    text_to_print = timestamp_text + ' ' + text_to_print
+                text_to_print = text_to_print.strip()
+                print('\033[1m{}\033[0m'.format(text_to_print))
+                text_to_send += task.translated_text
+            text_to_send = text_to_send.strip()
+            if output_file_path:
+                _output_to_file(output_file_path, text_to_send)
+            if cqhttp_url:
+                _send_to_cqhttp(cqhttp_url, cqhttp_token, proxies, text_to_send)
+            if discord_webhook_url:
+                _send_to_discord(discord_webhook_url, proxies, text_to_send)
+            if telegram_token and telegram_chat_id:
+                _send_to_telegram(telegram_token, telegram_chat_id, proxies, text_to_send)
