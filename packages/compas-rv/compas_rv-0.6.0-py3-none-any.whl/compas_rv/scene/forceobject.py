e53d69311e840a0c5f073efb3d7b3a7843154b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 03:05:24 +0000

    Add package compas-rv/0.6.0/compas_rv-0.6.0-py3-none-any.whl

---
+++ packages/compas-rv/compas_rv-0.6.0-py3-none-any.whl/compas_rv/scene/forceobject.py
@@ -0,0 +1,37 @@
+from compas.colors import Color
+from compas.scene.descriptors.colordict import ColorDictAttribute
+from compas_rv.datastructures import ForceDiagram
+from compas_rv.datastructures import FormDiagram
+from compas_rv.session import RVSession
+
+from .diagramobject import RhinoDiagramObject
+
+
+class RhinoForceObject(RhinoDiagramObject):
+    session = RVSession()
+    diagram: ForceDiagram
+
+    vertexcolor = ColorDictAttribute(default=Color.blue())
+    edgecolor = ColorDictAttribute(default=Color.blue().darkened(50))
+    facecolor = ColorDictAttribute(default=Color.blue().lightened(25))
+
+    # =============================================================================
+    # Properties
+    # =============================================================================
+
+    def forces(self):
+        primal: FormDiagram = self.diagram.primal
+        edges = [self.diagram.primal_edge(edge) for edge in self.edges()]
+        return primal.edges_attribute("_f", keys=edges)
+
+    # =============================================================================
+    # Clear
+    # =============================================================================
+
+    # =============================================================================
+    # Draw
+    # =============================================================================
+
+    # =============================================================================
+    # Redraw
+    # =============================================================================
