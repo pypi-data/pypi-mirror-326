c90176d29c03cb773a5f64216db0e449fcc9bd85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 04:03:53 +0000

    Add package wavesongs/1.0.1b0/wavesongs-1.0.1b0.tar.gz

---
+++ packages/wavesongs/wavesongs-1.0.1b0.tar.gz/wavesongs-1.0.1b0/README.md
@@ -0,0 +1,165 @@
+<div align='center' style="margin: 20px 0 0px 0">
+
+   <img src="./assets/logo.png" alt="WaveSongs logo" style="max-width: 100%; height: 200px;">
+
+   <div class="text-container" style="flex: 2;">
+      <h1 style="margin: 0; padding: 10px 0 0px 0; border-bottom: 0">WaveSongs</h1>
+      <p style="margin: 0; padding: 0px 0 10px 0;">A Python package for birdsong synthesis and bioacoustic analysis</p>
+   </div> 
+</div>
+
+
+<!-- <div class="container" style="display: flex; align-items: center; justify-content: space-between; max-width: 70%; min-width: 400px; margin: 0 auto; padding: 10px 0 15px 0; border-bottom: 1px solid">
+    <div class="image-container" style="flex: 1; padding: 0 0 0 10px;">
+        <img src="./assets/logo.png" alt="WaveSongs logo" style="max-width: 100%; height: auto; display: block;">
+    </div>
+    <div class="text-container" style="flex: 2;">
+        <h1 style="margin: 0; padding: 0 0 5px 0; border-bottom: 0">WaveSongs</h1>
+        <p style="margin: 0; padding: 5px 0 0 0;">A Python package for birdsong synthesis and bioacoustic analysis</p>
+    </div>
+</div> -->
+
+<div align='center' style="margin: 20px 0 50px 0">
+
+![version](https://img.shields.io/badge/version-1.0.1-008000)
+[![License: GPL v3](https://img.shields.io/badge/license-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+![Python 3.10+](https://img.shields.io/badge/python->=3.10-blue.svg)
+![Open Source](https://img.shields.io/badge/open%20source-♡-lightgrey)
+
+[Overview](#overview) •
+[Installation](#installation) •
+[Quick Start](#quick-start) •
+[Contribute](#-contribute) •
+[References](#-references)
+
+</div>
+
+---
+
+
+## 🔎 Overview
+
+WaveSongs implements the [motor gestures model for birdsong](http://www.lsd.df.uba.ar/papers/simplemotorgestures.pdf) developed by [Gabo Mindlin](https://scholar.google.com.ar/citations?user=gMzZPngAAAAJ&hl=en) to generate synthetic birdsongs through numerical optimization. By leveraging **fundamental frequency (FF)** and **spectral content index (SCI)** as key parameters, the package solves a minimization problem using [SciPy](https://docs.scipy.org/doc/scipy/tutorial/optimize.html) and performs audio analysis with [librosa](https://librosa.org/).
+
+Validated against field recordings of *Zonotrichia Capensis*, *Ocellated Tapaculo*, and *Mimus Gilvus*, the model achieves **<5% relative error in FF reconstruction** compared to empirical data.
+
+
+## ⚒️ Installation
+
+### Prerequisites
+- [Python](https://www.python.org/) ≥ 3.10
+- [Git](https://git-scm.com/)
+
+### Steps
+1. **Clone the repository**:
+   ```bash
+   git clone https://github.com/wavesongs/wavesongs
+   cd wavesongs
+   ```
+
+2. **Set up a virtual environment** (choose one method):
+
+   #### Using `venv`
+   ```bash
+   python -m venv venv
+   # Activate on Linux/macOS
+   source venv/bin/activate
+   # Activate on Windows
+   .\venv\Scripts\activate
+   ```
+
+   #### Using Conda
+   ```bash
+   conda create -n wavesongs python=3.12
+   conda activate wavesongs
+   ```
+
+3. **Install dependencies**:
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+4. **Install WaveSongs** in editable mode:
+   ```bash
+   pip install -e .
+   ```
+
+
+## 🚀 Gettint Started
+
+Explore the [Tutorial Notebook](./Tutorial.ipynb) to generate synthetic birdsongs and analyze acoustic features. Here is an example of simple code to import and display an audio.
+
+```python
+# select matplotlib backend for notebook, enable interactive plots
+%matplotlib ipympl
+
+from wavesongs.utils.paths import ProjDirs       # project files manager
+from wavesongs.objects.song import Song          # song objects
+from wavesongs.objects.syllable import Syllable  # syllable objects
+from wavesongs.utils import plots                # plotter
+
+proj_dirs = ProjDirs(audios="./assets/audio", results="./assets/results")
+
+# define the song and compute its acoustical features
+copeton_song = Song(proj_dirs, file_id="574179401")
+copeton_song.acoustical_features(umbral_FF=1.4, NN=256)
+
+# display the song
+plots.spectrogram_waveform(copeton_song, save=False)
+```
+
+![Sample Output](./assets/results/images/574179401%20-%20Zonotrichia%20Capensis-Song.png)
+
+For advanced usage (e.g., custom gestures, parameter tuning), refer to the [Documentation](./docs/).
+
+
+## 🎶 Data Integration
+
+Pre-processed field recordings from [Xeno Canto](https://xeno-canto.org/) and [eBird](https://ebird.org/home) are included in `./assets/audio`. To use custom recordings place `.wav` or `.mp3` files in `./assets/audio/` or define the audios path with the `ProjDirs` class.
+
+
+## 📜 License
+
+WaveSongs is licensed under the [GNU General Public License v3.0](./LICENSE).
+
+## 📒 Citation
+
+If this work contributes to your research, please cite:
+
+```bibtex
+@software{aguilera_wavesongs_2025,
+    author = {Aguilera Novoa, Sebastián},
+    title = {WaveSongs: Computational Birdsong Synthesis},
+    year = {2025},
+    publisher = {GitHub},
+    journal = {GitHub Repository},
+    url = {https://github.com/wavesongs/wavesongs}
+}
+```
+
+
+## 🌱 Contribute
+
+We welcome contributions! See our roadmap:
+
+- [ ] **Integrate Xeno Canto API** for direct dataset downloads
+- [ ] **Add ROIs analysis** using `scikit-maad`
+- [ ] **Improve FF parametrization** for non-linear gestures
+
+To report issues or suggest features, open a [GitHub Issue](https://github.com/wavesongs/wavesongs/issues).
+
+
+## 📚 References
+
+### Core Methodology
+1. Mindlin, G. B., & Laje, R. (2005). *The Physics of Birdsong*. Springer. [DOI](https://doi.org/10.1007/3-540-28249-1)
+2. Amador, A., et al. (2013). Elemental gesture dynamics in song premotor neurons. *Nature*. [DOI](https://doi.org/10.1038/nature11967)
+
+### Software
+- [Librosa](https://librosa.org/) • Audio analysis
+- [SciPy](https://scipy.org/) • Optimization routines
+- [scikit-maad](https://github.com/scikit-maad/scikit-maad) • Soundscape metrics
+
+### Data Sources
+- [Xeno Canto](https://xeno-canto.org/) • Field recordings
+- [eBird](https://ebird.org/) • Species metadata
---
+++ packages/wavesongs/wavesongs-1.0.1b0.tar.gz/wavesongs-1.0.1b0/wavesongs/utils/paths.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Create project directory trees, assign path variables, and get paths for
+different types of output files
+"""
+import numpy as np
+import pandas as pd
+
+from os import makedirs
+from librosa import load
+from os.path import isdir, basename
+from pathlib import Path, PosixPath
+from typing import Union, List, AnyStr
+
+_CATALOG_LABEL = "ML Catalog Number"
+_AUDIO_FORMATS = (".mp3", ".wav")
+
+#%%
+class ProjDirs:
+    """
+
+    Parameters
+    ----------
+        audios : str ='./assets/audio'
+            Folder path where the audio samples are saved.
+        results: str = "./assets/results"
+            Folder path for the generated files and data.
+        metadata: str = "spreadsheet.csv"
+            Name of the csv file with the metadata of the audios. 
+            Usually given by the data provider.
+
+    Attributes
+    ----------
+
+    Example
+    -------
+        >>> proj_dirs = ProjDirs(
+        >>>     "./assets/audio", "./assets/results", "spreadsheet.csv"
+        >>> )
+    """
+    # %%
+    def __init__(
+        self,
+        audios: AnyStr = "./assets/audio",
+        results: AnyStr = "./assets/results",
+        metadata: AnyStr = "spreadsheet.csv",
+        catalog: bool = False
+    ):
+        """Constructor"""
+        self.AUDIOS = Path(audios)
+        self.RESULTS = Path(results)
+
+        self.MG_param = self.RESULTS / "mg_params"
+        self.IMAGES = self.RESULTS / "images"
+        
+        self.examples = self.RESULTS / "audios"
+        self.SPREADSHEET = self.AUDIOS / metadata
+        self.CATALOG = catalog
+
+        # create folder in case they do not exist
+        if not isdir(self.RESULTS):
+            makedirs(self.RESULTS)
+        if not isdir(self.MG_param):
+            makedirs(self.MG_param)
+        if not isdir(self.IMAGES):
+            makedirs(self.IMAGES)
+        if not isdir(self.examples):
+            makedirs(self.examples)
+
+        # Check if there is a metadata spreadsheet file inside audios folder
+        spreadsheet_file = list(Path(self.AUDIOS).glob("*" + metadata))
+        if len(spreadsheet_file) > 0 and self.CATALOG==True:
+            self.CATALOG = True 
+            self.CATALOG_LABEL = _CATALOG_LABEL
+
+        self.find_audios()
+    # %%
+    def find_audios(self) -> Union[List, pd.DataFrame]:
+        """
+        Search for all audios, mp3 and wav type, in the audios folder. 
+        
+        Parameters
+        ----------
+            None
+
+        Return
+        ------
+            files_names : list
+                List with the audios files names
+
+        Notes
+        -----
+            If the audios folder contains a metadata file, spreadsheet.csv,
+            the method will return a dataframe. However, the attribute
+            files_names always is present.
+
+        Example
+        -------
+            >>>
+        """
+        all_filles = Path(self.AUDIOS).glob("**/*")
+        self.files = [a for a in all_filles if a.suffix in _AUDIO_FORMATS]
+        self.files_names = [basename(f) for f in self.files]
+        self.no_files = len(self.files_names)
+
+        if self.CATALOG is True:
+            self.data = pd.read_csv(self.SPREADSHEET, encoding_errors="ignore")
+            self.data.dropna(axis=0, how="all", inplace=True)
+            # self.data = data.convert_dtypes()
+            self.data = self.data.astype({self.CATALOG_LABEL: "str"})
+            found_files = [
+                (
+                    str(self.AUDIOS) + f"/{file}.mp3"
+                    if file + ".mp3" in self.files_names
+                    else str(self.AUDIOS) + f"/{file}.wav"
+                )
+                for file in self.data[self.CATALOG_LABEL]
+            ]
+            self.data["File Path"] = found_files
+            self.no_files = len(self.data)
+
+            return self.data
+
+        return self.files_names
+    # %%
+    def audios_info(self) -> None:
+        """
+        Display information about the audios folder: audios path and 
+        number of audios in the folder.
+
+        Parameters
+        ----------
+            None
+
+        Return
+        ------
+            None
+
+        Example
+        -------
+            >>>
+        """
+        print(f"Audios path: {self.AUDIOS}\n")
+        print("The folder has {} audio samples:".format(self.no_files))
+
+        if self.CATALOG:
+            print(self.data)
+        else:
+            for file in self.files_names:
+                print("  - " + file)
+
+    def find_audio(self, id: AnyStr) -> PosixPath:
+        """
+        Find an audio in the audios folder by the id or filename
+
+        Parameters
+        ----------
+            id : str
+                Whole filename of a part of it. Usually, the catalog number. 
+
+        Return
+        ------
+            path : PosixPath
+                Path location of the audio. 
+
+        Example
+        -------
+            >>>
+        """
+        if self.CATALOG:
+            id_df = self.data.loc[self.data[self.CATALOG_LABEL] == id]
+            path = PosixPath(id_df["File Path"].values[0])
+        else:
+            path = [
+                self.files[i]
+                for i in range(len(self.files))
+                if id in self.files_names[i]
+            ][0]
+        return path
+
+    # #%%
+    # def ImportParameters1(self, XC=None, no_file=None, no_syllable=None, name=None):
+    #     self.data_param = self.MG_Files()
+
+    #     if name is not None and XC is None and no_file is None :
+    #         df = self.data_param["name"].str.contains(name,case=False)
+
+    #     if XC is not None and no_file is None and name is None:
+    #         df = self.data_param[self.data_param['id_XC'] == XC]
+    #     if no_file is not None and XC is None and name is None:
+    #         df = self.data_param.iloc[no_file]
+    #     if no_file is None and XC is None:
+    #         df = self.data_param
+    #     if no_file is None and XC is None and name is None:
+    #         df = self.data_param
+
+    #     if no_syllable is not None:
+    #         df = df[df['no_syllable'] == str(no_syllable)]
+    #         coef = pd.read_csv(df["coef_path"].values[0]).rename(columns={"Unnamed: 0":"parameter"})
+    #         tlim = pd.Series({"t_ini":coef.iloc[-2].value, "t_end":coef.iloc[-1].value})
+    #         df = pd.concat([df, tlim]).reset_index()
+
+    #         return df#, coef#, motor_gesture
+    #     else:                               # if syllables is Nonex
+    #         coefs, type, out, tlim, NN, umbral_FF, country, state = [], [], [], [], [], [], [], []
+    #         for i in df.index:
+    #             coef = pd.read_csv(self.data_param.iloc[i]["coef_path"], index_col="Unnamed: 0", engine='python')#, encoding = "utf-8") #cp1252
+    #             tlim.append([float(coef.iloc[7].value), float(coef.iloc[8].value)])
+    #             NN.append(int(coef.iloc[9].value))
+    #             umbral_FF.append(float(coef.iloc[10].value))
+    #             type.append(coef.iloc[11].value)
+    #             country.append(coef.iloc[12].value)
+    #             state.append(coef.iloc[13].value)
+    #             coefs.append(coef.iloc[:7].astype('float64'))
+    #         tlim = np.array(tlim)
+
+    #         df = pd.DataFrame({'id_XC':df['id_XC'], 'no_syllable':df['no_syllable'],
+    #         'id':df['id'], 'name':df['name'], 'coef_path':df['coef_path'], 'param_path':df['param_path'],
+    #         'audio_path':df['audio_path'], 's':df['s'], 'fs':df['fs'], 'file_name':df['file_name'],
+    #         't_ini':tlim[:,0], 't_end':tlim[:,1], 'NN':NN, 'umbral_FF':umbral_FF, 'coef':coefs, 'type':type, 'country':country, 'state':state},
+    #         index=df.index)
+
+    #         out = [df.iloc[i] for i in range(len(df.index))]
+    #         self.df = df.reset_index(drop=True, inplace=False)
+
+    #         print("{} files were found.".format(len(df.index)))
+    #         return out, df
+    # #%%
+    # def ImportParameters(self, no_syllable=None, country_filter=None):
+    #     df = self.MG_Files()
+    #     coefs, type, out, tlim, NN, umbral_FF, country, state = [], [], [], [], [], [], [], []
+    #     for i in df.index:
+    #         coef = pd.read_csv(self.data_param.iloc[i]["coef_path"], index_col="Unnamed: 0", engine='python').T#, encoding = "utf-8") #cp1252
+    #         tlim.append([float(coef["t_ini"].value), float(coef["t_end"].value)])
+    #         NN.append(int(coef["NN"].value))
+    #         umbral_FF.append(float(coef["umbral_FF"].value))
+    #         type.append(coef["type"].value)
+    #         country.append(coef["country"].value)
+    #         state.append(coef["state"].value)
+    #         coefs.append(coef[["a0","a1","a2","b0","b1","b2","gm"]].values[0]) # coef.iloc[:7].astype('float64')
+    #     tlim = np.array(tlim)
+
+    #     df = pd.DataFrame({'id_XC':df['id_XC'], 'no_syllable':df['no_syllable'],
+    #     'id':df['id'], 'name':df['name'], 'coef_path':df['coef_path'],
+    #     # 'param_path':df['param_path'], 's':df['s'], 'fs':df['fs'],
+    #     'audio_path':df['audio_path'], 'file_name':df['file_name'],
+    #     't_ini':tlim[:,0], 't_end':tlim[:,1], 'NN':NN, 'umbral_FF':umbral_FF, 'coef':coefs, 'type':type, 'country':country, 'state':state},
+    #     index=df.index)
+
+    #     self.df = df.reset_index(drop=True, inplace=False)
+    #     print("{} files were found.".format(len(self.df.index)))
+
+    #     if country_filter is not None:
+    #         self.df = self.df[self.df["country"]==country_filter]
+    #     if no_syllable is not None:
+    #         self.df = self.df[self.df["no_syllable"]==str(no_syllable)]
+
+    #     return self.df
+    # #%%
+    # def MG_Files(self):
+    #     self.MG_coef = list(self.MG_param.glob("*MG.csv"))
+    #     MG_coef_splited  = [relpath(MG, self.MG_param) for MG in self.MG_coef]
+    #     MG_coef_splited  = [relpath(MG, self.MG_param).replace(" ","").split("-") for MG in self.MG_coef]
+
+    #     id_XC = [x[0] for x in MG_coef_splited]
+    #     no_syllables = [x[-2] for x in MG_coef_splited]
+    #     id = [x[-3] for x in MG_coef_splited]
+    #     name = [x[1]+"-"+x[2] for x in MG_coef_splited]
+    #     audios = [list(self.AUDIOS.glob(id+"*"))[0]  for id  in id_XC]
+    #     file_name = [relpath(audio, self.AUDIOS) for audio in audios]
+
+    #     self.data_param = pd.DataFrame({'id_XC':id_XC,
+    #                                     'no_syllable': no_syllables,
+    #                                     'id': id,
+    #                                     'name':name,
+    #                                     "coef_path":self.MG_coef,
+    #                                     "audio_path":audios,
+    #                                     "file_name":file_name})
+    #                                             #"coef_path":self.MG_coef, "s":ss, "fs":fss,
+
+    #     return self.data_param
