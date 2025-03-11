992f33db67910ca2a66676a86bd1d03192aa8d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:35:39 +0000

    Add package codosdk/1.0.14/codosdk-1.0.14.tar.gz

---
+++ packages/codosdk/codosdk-1.0.14.tar.gz/codosdk-1.0.14/websdk2/apis/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .admin_apis import AdminAPIS
+from .task_apis import TaskAPIS
+from .kerrigan_apis import KerriganAPIS
+from .mgv4_apis import AdminV4APIS
+from .cmdb_apis import CMDBAPIS
+from .agent_apis import AgentAPIS
