f902dde8ac10fa9c04178062ad831e281787e3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:04:12 +0000

    Add package vkdispatch/0.0.20/vkdispatch-0.0.20-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/vkdispatch/vkdispatch-0.0.20-cp39-cp39-macosx_10_9_x86_64.whl/vkdispatch/__init__.py
@@ -0,0 +1,71 @@
+from .base.errors import check_for_errors
+from .base.errors import check_for_compute_stage_errors
+
+from .base.init import DeviceInfo
+from .base.init import LogLevel
+from .base.init import get_devices
+from .base.init import initialize
+from .base.init import is_initialized
+from .base.init import log, log_error, log_warning, log_info, log_verbose
+
+from .base.dtype import dtype
+from .base.dtype import float32, int32, uint32, complex64
+from .base.dtype import vec2, vec3, vec4, ivec2, ivec3, ivec4, uvec2, uvec3, uvec4
+from .base.dtype import mat2, mat4
+from .base.dtype import is_scalar, is_complex, is_vector, is_matrix, is_dtype
+from .base.dtype import to_numpy_dtype, from_numpy_dtype, to_vector
+
+from .base.context import get_context
+from .base.context import get_context_handle
+from .base.context import make_context
+from .base.context import is_context_initialized
+from .base.context import stage_for_postinit
+
+from .base.buffer import asbuffer
+from .base.buffer import Buffer
+
+from .base.image import image_format
+from .base.image import image_type
+from .base.image import image_view_type
+from .base.image import Image
+from .base.image import Image1D
+from .base.image import Image2D
+from .base.image import Image2DArray
+from .base.image import Image3D
+from .base.image import Sampler
+from .base.image import Filter
+from .base.image import AddressMode
+from .base.image import BorderColor
+
+from .base.compute_plan import ComputePlan
+
+from .base.fft_plan import FFTPlan
+
+from .base.transfer_operations import stage_transfer_copy_buffers
+from .base.descriptor_set import DescriptorSet
+
+from .base.command_list import CommandList
+
+from .execution_pipeline.buffer_builder import BufferUsage, BufferedStructEntry, BufferBuilder
+
+from .execution_pipeline.command_stream import CommandStream
+from .execution_pipeline.command_stream import global_cmd_stream, set_global_cmd_stream, default_cmd_stream
+
+from .execution_pipeline.fft_dispatcher import fft
+from .execution_pipeline.fft_dispatcher import ifft
+from .execution_pipeline.fft_dispatcher import reset_fft_plans
+
+from .shader_generation.signature import ShaderArgumentType
+from .shader_generation.signature import ShaderArgument
+from .shader_generation.signature import ShaderSignature
+
+from .shader_generation.shader_object import ShaderObject
+from .shader_generation.shader_object import ExectionBounds
+from .shader_generation.shader_object import LaunchParametersHolder
+
+from .shader_generation.decorator import shader
+
+from .shader_generation.reductions import map_reduce
+
+
+__version__ = "0.0.20"
