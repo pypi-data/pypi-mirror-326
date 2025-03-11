c7b093bededbfb1062bad4d7582e576633952b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:24:58 +0000

    Add package langfuse-pydantic-ai/0.0.3/langfuse_pydantic_ai-0.0.3.tar.gz

---
+++ packages/langfuse-pydantic-ai/langfuse_pydantic_ai-0.0.3.tar.gz/langfuse_pydantic_ai-0.0.3/langfuse_pydantic_ai/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for langfuse-pydantic."""
+
+__author__ = "wh1isper"
+__email__ = "jizhongsheng957@gmail.com"
+__version__ = "0.0.3"
+
+
+from ._warpper import observe_model, observed_agent, use_observed_agent
---
+++ packages/langfuse-pydantic-ai/langfuse_pydantic_ai-0.0.3.tar.gz/langfuse_pydantic_ai-0.0.3/tests/test_langfuse_pydantic.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+def test_langfuse_pydantic_ai():
+    assert True
+
+
+if __name__ == "__main__":
+    pytest.main()
