2d195be7a689627479328bdf0f2bb8377d11ae7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:25:04 +0000

    Add package ai-marketplace-monitor/0.6.9/ai_marketplace_monitor-0.6.9.tar.gz

---
+++ packages/ai-marketplace-monitor/ai_marketplace_monitor-0.6.9.tar.gz/ai_marketplace_monitor-0.6.9/src/ai_marketplace_monitor/ai.py
@@ -0,0 +1,295 @@
+import re
+import time
+from dataclasses import asdict, dataclass, field
+from enum import Enum
+from logging import Logger
+from typing import Any, ClassVar, Generic, Optional, Type, TypeVar
+
+from openai import OpenAI  # type: ignore
+from rich.pretty import pretty_repr
+
+from .listing import Listing
+from .marketplace import TItemConfig
+from .utils import CacheType, CounterItem, DataClassWithHandleFunc, cache, counter, hilight
+
+
+class AIServiceProvider(Enum):
+    OPENAI = "OpenAI"
+    DEEPSEEK = "DeepSeek"
+    OLLAMA = "Ollama"
+
+
+@dataclass
+class AIResponse:
+    score: int
+    comment: str
+
+    NOT_EVALUATED: ClassVar = "Not evaluated by AI"
+
+    @property
+    def conclusion(self: "AIResponse") -> str:
+        return {
+            1: "No match",
+            2: "Potential match",
+            3: "Poor match",
+            4: "Good match",
+            5: "Great deal",
+        }[self.score]
+
+    @property
+    def style(self: "AIResponse") -> str:
+        if self.score < 3:
+            return "fail"
+        if self.score > 3:
+            return "succ"
+        return "name"
+
+    @classmethod
+    def from_cache(
+        cls: Type["AIResponse"], listing: Listing, item_config: TItemConfig
+    ) -> Optional["AIResponse"]:
+        res = cache.get((CacheType.AI_INQUIRY.value, item_config.hash, listing.hash))
+        if res is None:
+            return None
+        return AIResponse(**res)
+
+    def to_cache(self: "AIResponse", listing: Listing, item_config: TItemConfig) -> None:
+        cache.set(
+            (CacheType.AI_INQUIRY.value, item_config.hash, listing.hash),
+            asdict(self),
+            tag=CacheType.AI_INQUIRY.value,
+        )
+
+
+@dataclass
+class AIConfig(DataClassWithHandleFunc):
+    # this argument is required
+
+    api_key: str | None = None
+    provider: str | None = None
+    model: str | None = None
+    base_url: str | None = None
+    max_retries: int = 10
+    timeout: int | None = None
+
+    def handle_provider(self: "AIConfig") -> None:
+        if self.provider is None:
+            return
+        if self.provider.lower() not in [x.value.lower() for x in AIServiceProvider]:
+            raise ValueError(
+                f"""AIConfig requires a valid service provider. Valid providers are {hilight(", ".join([x.value for x in AIServiceProvider]))}"""
+            )
+
+    def handle_api_key(self: "AIConfig") -> None:
+        if self.api_key is None:
+            return
+        if not isinstance(self.api_key, str):
+            raise ValueError("AIConfig requires a string api_key.")
+        self.api_key = self.api_key.strip()
+
+    def handle_max_retries(self: "AIConfig") -> None:
+        if not isinstance(self.max_retries, int) or self.max_retries < 0:
+            raise ValueError("AIConfig requires a positive integer max_retries.")
+
+    def handle_timeout(self: "AIConfig") -> None:
+        if self.timeout is None:
+            return
+        if not isinstance(self.timeout, int) or self.timeout < 0:
+            raise ValueError("AIConfig requires a positive integer timeout.")
+
+
+@dataclass
+class OpenAIConfig(AIConfig):
+    def handle_api_key(self: "OpenAIConfig") -> None:
+        if self.api_key is None:
+            raise ValueError("OpenAI requires a string api_key.")
+
+
+@dataclass
+class DeekSeekConfig(OpenAIConfig):
+    pass
+
+
+@dataclass
+class OllamaConfig(OpenAIConfig):
+    api_key: str | None = field(default="ollama")  # required but not used.
+
+    def handle_base_url(self: "OllamaConfig") -> None:
+        if self.base_url is None:
+            raise ValueError("Ollama requires a string base_url.")
+
+    def handle_model(self: "OllamaConfig") -> None:
+        if self.model is None:
+            raise ValueError("Ollama requires a string model.")
+
+
+TAIConfig = TypeVar("TAIConfig", bound=AIConfig)
+
+
+class AIBackend(Generic[TAIConfig]):
+    def __init__(self: "AIBackend", config: AIConfig, logger: Logger | None = None) -> None:
+        self.config = config
+        self.logger = logger
+        self.client: OpenAI | None = None
+
+    @classmethod
+    def get_config(cls: Type["AIBackend"], **kwargs: Any) -> TAIConfig:
+        raise NotImplementedError("get_config method must be implemented by subclasses.")
+
+    def connect(self: "AIBackend") -> None:
+        raise NotImplementedError("Connect method must be implemented by subclasses.")
+
+    def get_prompt(self: "AIBackend", listing: Listing, item_config: TItemConfig) -> str:
+        prompt = (
+            f"""A user wants to buy a {item_config.name} from Facebook Marketplace. """
+            f"""Search keywords: "{'" and "'.join(item_config.keywords)}", """
+        )
+        if item_config.description:
+            prompt += f"""Description: "{item_config.description}", """
+        #
+        max_price = item_config.max_price or 0
+        min_price = item_config.min_price or 0
+        if max_price and min_price:
+            prompt += f"""Price range: {min_price} to {max_price}. """
+        elif max_price:
+            prompt += f"""Max price {max_price}. """
+        elif min_price:
+            prompt += f"""Min price {min_price}. """
+        #
+        if item_config.exclude_keywords:
+            prompt += f"""Exclude keywords "{'" and "'.join(item_config.exclude_keywords)}"."""
+        if item_config.exclude_by_description:
+            prompt += f"""Exclude description with: "{'" and "'.join(item_config.exclude_by_description)}"."""
+        #
+        prompt += (
+            """\n\nThe user found a listing titled "{listing.title}" in {listing.condition} condition, """
+            f"""priced at {listing.price}, located in {listing.location}, """
+            f"""posted at {listing.post_url} with description "{listing.description}"\n\n"""
+            "Evaluate how well this listing matches the user's criteria. Assess the description, MSRP, model year, "
+            "condition, and seller's credibility. Rate from 1 to 5 based on the following: \n"
+            "1 - No match: Missing key details, wrong category/brand, or suspicious activity (e.g., external links).\n"
+            "2 - Potential match: Lacks essential info (e.g., condition, brand, or model); needs clarification.\n"
+            "3 - Poor match: Some mismatches or missing details; acceptable but not ideal.\n"
+            "4 - Good match: Mostly meets criteria with clear, relevant details.\n"
+            "5 - Great deal: Fully matches criteria, with excellent condition or price.\n"
+            "Conclude with:\n"
+            '"Rating [1-5]: [summary]"\n'
+            "where [1-5] is the rating and [summary] is a brief recommendation (max 30 words)."
+        )
+        if self.logger:
+            self.logger.debug(f"""{hilight("[AI-Prompt]", "info")} {prompt}""")
+        return prompt
+
+    def evaluate(self: "AIBackend", listing: Listing, item_config: TItemConfig) -> AIResponse:
+        raise NotImplementedError("Confirm method must be implemented by subclasses.")
+
+
+class OpenAIBackend(AIBackend):
+    default_model = "gpt-4o"
+    # the default is f"https://api.openai.com/v1"
+    base_url: str | None = None
+
+    @classmethod
+    def get_config(cls: Type["OpenAIBackend"], **kwargs: Any) -> OpenAIConfig:
+        return OpenAIConfig(**kwargs)
+
+    def connect(self: "OpenAIBackend") -> None:
+        if self.client is None:
+            self.client = OpenAI(
+                api_key=self.config.api_key,
+                base_url=self.config.base_url or self.base_url,
+                timeout=self.config.timeout,
+            )
+            if self.logger:
+                self.logger.info(f"""{hilight("[AI]", "name")} {self.config.name} connected.""")
+
+    def evaluate(self: "OpenAIBackend", listing: Listing, item_config: TItemConfig) -> AIResponse:
+        # ask openai to confirm the item is correct
+        counter.increment(CounterItem.AI_QUERY)
+        prompt = self.get_prompt(listing, item_config)
+        res: AIResponse | None = AIResponse.from_cache(listing, item_config)
+        if res is not None:
+            if self.logger:
+                self.logger.info(
+                    f"""{hilight("[AI]", "name")} {self.config.name} has already evaluated {hilight(listing.title)}."""
+                )
+            return res
+
+        self.connect()
+
+        retries = 0
+        while retries < self.config.max_retries:
+            self.connect()
+            assert self.client is not None
+            try:
+                response = self.client.chat.completions.create(
+                    model=self.config.model or self.default_model,
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": "You are a helpful assistant that can confirm if a user's search criteria matches the item he is interested in.",
+                        },
+                        {"role": "user", "content": prompt},
+                    ],
+                    stream=False,
+                )
+                break
+            except Exception as e:
+                if self.logger:
+                    self.logger.error(
+                        f"""{hilight("[AI-Error]", "fail")} {self.config.name} failed to evaluate {hilight(listing.title)}: {e}"""
+                    )
+                retries += 1
+                # try to initiate a connection
+                self.client = None
+                time.sleep(5)
+
+        # check if the response is yes
+        if self.logger:
+            self.logger.debug(f"""{hilight("[AI-Response]", "info")} {pretty_repr(response)}""")
+
+        answer = response.choices[0].message.content or ""
+        if (
+            answer is None
+            or not answer.strip()
+            or re.search(r"Rating[:\s]*[1-5]", answer, re.DOTALL) is None
+        ):
+            counter.increment(CounterItem.FAILED_AI_QUERY)
+            raise ValueError(f"Empty or invalid response from {self.config.name}: {response}")
+
+        lines = answer.split("\n")
+        # if any of the lines contains "Rating: ", extract the rating from it.
+        score: int = 1
+        comment = ""
+        for line in lines:
+            matched = re.match(r".*Rating[:\s]*([1-5])[:\s]*(.*)", line)
+            if matched:
+                score = int(matched.group(1))
+                comment = matched.group(2).strip()
+                break
+
+        res = AIResponse(score, comment)
+        res.to_cache(listing, item_config)
+        if self.logger:
+            self.logger.info(
+                f"""{hilight("[AI]", res.style)} {self.config.name} concludes {hilight(f"{res.conclusion} ({res.score}): {res.comment}", res.style)} for listing {hilight(listing.title)}."""
+            )
+        counter.increment(CounterItem.NEW_AI_QUERY)
+        return res
+
+
+class DeepSeekBackend(OpenAIBackend):
+    default_model = "deepseek-chat"
+    base_url = "https://api.deepseek.com"
+
+    @classmethod
+    def get_config(cls: Type["DeepSeekBackend"], **kwargs: Any) -> DeekSeekConfig:
+        return DeekSeekConfig(**kwargs)
+
+
+class OllamaBackend(OpenAIBackend):
+    default_model = "deepseek-r1:14b"
+
+    @classmethod
+    def get_config(cls: Type["OllamaBackend"], **kwargs: Any) -> OllamaConfig:
+        return OllamaConfig(**kwargs)
---
+++ packages/ai-marketplace-monitor/ai_marketplace_monitor-0.6.9.tar.gz/ai_marketplace_monitor-0.6.9/src/ai_marketplace_monitor/utils.py
@@ -0,0 +1,344 @@
+import hashlib
+import re
+import time
+from dataclasses import asdict, dataclass, fields
+from enum import Enum
+from pathlib import Path
+from typing import Any, Dict, List, Tuple, TypeVar
+
+import parsedatetime  # type: ignore
+import rich
+from diskcache import Cache  # type: ignore
+from rich.pretty import pretty_repr
+
+try:
+    from pynput import keyboard  # type: ignore
+
+    pynput_installed = True
+except ImportError:
+    # some platforms are not supported
+    pynput_installed = False
+
+import rich.pretty
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
+
+# home directory for all settings and caches
+amm_home = Path.home() / ".ai-marketplace-monitor"
+amm_home.mkdir(parents=True, exist_ok=True)
+
+cache = Cache(amm_home)
+
+
+TConfigType = TypeVar("TConfigType", bound="DataClassWithHandleFunc")
+
+
+class SleepStatus(Enum):
+    NOT_DISRUPTED = 0
+    BY_KEYBOARD = 1
+    BY_FILE_CHANGE = 2
+
+
+class NotificationStatus(Enum):
+    NOT_NOTIFIED = 0
+    EXPIRED = 1
+    NOTIFIED = 2
+    LISTING_CHANGED = 3
+
+
+class KeyboardMonitor:
+    confirm_character = "c"
+
+    def __init__(self: "KeyboardMonitor") -> None:
+        self._paused: bool = False
+        self._listener: keyboard.Listener | None = None
+        self._sleeping: bool = False
+        self._confirmed: bool | None = None
+
+    def start(self: "KeyboardMonitor") -> None:
+        if pynput_installed:
+            self._listener = keyboard.Listener(on_press=self.handle_key_press)
+            self._listener.start()  # start to listen on a separate thread
+
+    def stop(self: "KeyboardMonitor") -> None:
+        if self._listener:
+            self._listener.stop()  # stop the listener
+
+    def start_sleeping(self: "KeyboardMonitor") -> None:
+        self._sleeping = True
+
+    def confirm(self: "KeyboardMonitor", msg: str | None = None) -> bool:
+        self._confirmed = False
+        rich.print(
+            msg
+            or f"Press {hilight(self.confirm_character)} to enter interactive mode in 10 seconds: ",
+            end="",
+            flush=True,
+        )
+        try:
+            count = 0
+            while self._confirmed is False:
+                time.sleep(0.1)
+                if self._confirmed:
+                    return True
+                count += 1
+                # wait a total of 10s
+                if count > 100:
+                    break
+            return self._confirmed
+        finally:
+            # whether or not confirm is successful, reset paused and confirmed flag
+            self._paused = False
+            self._confirmed = None
+
+    def is_sleeping(self: "KeyboardMonitor") -> bool:
+        return self._sleeping
+
+    def is_paused(self: "KeyboardMonitor") -> bool:
+        return self._paused
+
+    def is_confirmed(self: "KeyboardMonitor") -> bool:
+        return self._confirmed is True
+
+    def set_paused(self: "KeyboardMonitor", paused: bool = True) -> None:
+        self._paused = paused
+
+    if pynput_installed:
+
+        def handle_key_press(
+            self: "KeyboardMonitor", key: keyboard.Key | keyboard.KeyCode | None
+        ) -> None:
+            # is sleeping, wake up
+            if self._sleeping:
+                if key == keyboard.Key.esc:
+                    self._sleeping = False
+                    return
+            # if waiting for confirmation, set confirm
+            if self._confirmed is False:
+                if getattr(key, "char", "") == self.confirm_character:
+                    self._confirmed = True
+                    return
+            # if being paused
+            if self.is_paused():
+                if key == keyboard.Key.esc:
+                    print("Still searching ... will pause as soon as I am done.")
+                    return
+            if key == keyboard.Key.esc:
+                print("Pausing search ...")
+                self._paused = True
+
+
+class CounterItem(Enum):
+    SEARCH_PERFORMED = "Search performed"
+    LISTING_EXAMINED = "Total listing examined"
+    LISTING_QUERY = "New listing fetched"
+    EXCLUDED_LISTING = "Listing excluded"
+    NEW_VALIDATED_LISTING = "New validated listing"
+    AI_QUERY = "Total AI Queries"
+    NEW_AI_QUERY = "New AI Queries"
+    FAILED_AI_QUERY = "Failed AI Queries)"
+    NOTIFICATIONS_SENT = "Notifications sent"
+    REMINDERS_SENT = "Reminders sent"
+
+
+class Counter:
+
+    def __init__(self: "Counter") -> None:
+        self.counters: Dict[str, int] = {}
+
+    def increment(self: "Counter", key: CounterItem, by: int = 1) -> None:
+        if key not in CounterItem:
+            raise ValueError(f"Invalid cunter key: {key}")
+
+        self.counters[key.value] = self.counters.get(key.value, 0) + by
+
+    def __str__(self: "Counter") -> str:
+        """Return pretty form of all non-zero counters"""
+        cnt = {
+            x.value: self.counters.get(x.value, 0)
+            for x in CounterItem
+            if self.counters.get(x.value, 0)
+        }
+        return pretty_repr(cnt)
+
+
+counter = Counter()
+
+
+def hashable(obj: Any) -> bool:
+    try:
+        hash(obj)
+        return True
+    except:
+        return False
+
+
+def freeze_dict(d: Dict[str, Any]) -> Tuple[Tuple[str, Any], ...]:
+    frozen_items = []
+    for key, value in d.items():
+        # Convert mutable values to immutable types
+        if isinstance(value, dict):
+            value = freeze_dict(value)  # Recursively freeze nested dictionaries
+        elif isinstance(value, list) and all(hashable(x) for x in value):
+            value = tuple(value)  # Convert lists to tuples
+        elif isinstance(value, set):
+            value = frozenset(value)  # Convert sets to frozensets
+        elif not hashable(value):
+            raise ValueError(
+                f"Cannot freeze dictionary for hashing: {key}={value} of type {type(value)}"
+            )
+        frozen_items.append((key, value))
+    return tuple(frozen_items)
+
+
+@dataclass
+class DataClassWithHandleFunc:
+    name: str
+
+    def __post_init__(self: "DataClassWithHandleFunc") -> None:
+        """Handle all methods that start with 'handle_' in the dataclass."""
+        for f in fields(self):
+            handle_method = getattr(self, f"handle_{f.name}", None)
+            if handle_method:
+                handle_method()
+
+    @property
+    def hash(self: "DataClassWithHandleFunc") -> str:
+        return str(hash(freeze_dict(asdict(self))))
+
+
+class CacheType(Enum):
+    LISTING_DETAILS = "listing-details"
+    AI_INQUIRY = "ai-inquiries"
+    USER_NOTIFIED = "user-notifications"
+
+
+def calculate_file_hash(file_paths: List[Path]) -> str:
+    """Calculate the SHA-256 hash of the file content."""
+    hasher = hashlib.sha256()
+    # they should exist, just to make sure
+    for file_path in file_paths:
+        if not file_path.exists():
+            raise FileNotFoundError(f"File not found: {file_path}")
+        #
+        with open(file_path, "rb") as file:
+            while chunk := file.read(8192):
+                hasher.update(chunk)
+    return hasher.hexdigest()
+
+
+def merge_dicts(dicts: list) -> dict:
+    """Merge a list of dictionaries into a single dictionary, including nested dictionaries.
+
+    :param dicts: A list of dictionaries to merge.
+    :return: A single merged dictionary.
+    """
+
+    def merge(d1: dict, d2: dict) -> dict:
+        for key, value in d2.items():
+            if key in d1:
+                if isinstance(d1[key], dict) and isinstance(value, dict):
+                    d1[key] = merge(d1[key], value)
+                elif isinstance(d1[key], list) and isinstance(value, list):
+                    d1[key].extend(value)
+                else:
+                    d1[key] = value
+            else:
+                d1[key] = value
+        return d1
+
+    result: Dict[str, Any] = {}
+    for dictionary in dicts:
+        result = merge(result, dictionary)
+    return result
+
+
+def normalize_string(string: str) -> str:
+    """Normalize a string by replacing multiple spaces (including space, tab, and newline) with a single space."""
+    return re.sub(r"\s+", " ", string).lower()
+
+
+def is_substring(var1: str | List[str], var2: str | List[str]) -> bool:
+    """Check if var1 is a substring of var2, after normalizing both strings. One of them can be a list of strings."""
+    if isinstance(var1, str):
+        if isinstance(var2, str):
+            return normalize_string(var1) in normalize_string(var2)
+        return any(normalize_string(var1) in normalize_string(s) for s in var2)
+    # var1 is a list, var2 must be a string
+    assert isinstance(var2, str)
+    return any(normalize_string(s1) in normalize_string(var2) for s1 in var1)
+
+
+class ChangeHandler(FileSystemEventHandler):
+    def __init__(self: "ChangeHandler", files: List[str]) -> None:
+        self.changed = False
+        self.files = files
+
+    def on_modified(self: "ChangeHandler", event: FileSystemEvent) -> None:
+        if not event.is_directory and event.src_path in self.files:
+            self.changed = True
+
+
+def doze(
+    duration: int, files: List[Path] | None = None, keyboard_monitor: KeyboardMonitor | None = None
+) -> SleepStatus:
+    """Sleep for a specified duration while monitoring the change of files.
+
+    Return:
+        0: if doze was done naturally.
+        1: if doze was disrupted by keyboard
+        2: if doze was disrupted by file change
+    """
+    event_handler = ChangeHandler([str(x) for x in (files or [])])
+    observers = []
+    if keyboard_monitor:
+        keyboard_monitor.start_sleeping()
+
+    for filename in files or []:
+        if not filename.exists():
+            raise FileNotFoundError(f"File not found: {filename}")
+        observer = Observer()
+        # we can only monitor a directory
+        observer.schedule(event_handler, str(filename.parent), recursive=False)
+        observer.start()
+        observers.append(observer)
+
+    start_time = time.time()
+    try:
+        while time.time() - start_time < duration:
+            if event_handler.changed:
+                return SleepStatus.BY_FILE_CHANGE
+            time.sleep(1)
+            if keyboard_monitor and not keyboard_monitor.is_sleeping():
+                return SleepStatus.BY_KEYBOARD
+        return SleepStatus.NOT_DISRUPTED
+    finally:
+        for observer in observers:
+            observer.stop()
+            observer.join()
+
+
+def extract_price(price: str) -> str:
+    if price.count("$") > 1:
+        match = re.search(r"\$\d+(?:\.\d{2})?", price)
+        price = match.group(0) if match else price
+    if "\xa0" in price:
+        price = price.split("\xa0")[0]
+    return price
+
+
+def convert_to_seconds(time_str: str) -> int:
+    cal = parsedatetime.Calendar(version=parsedatetime.VERSION_CONTEXT_STYLE)
+    time_struct, _ = cal.parse(time_str)
+    return int(time.mktime(time_struct) - time.mktime(time.localtime()))
+
+
+def hilight(text: str, style: str = "name") -> str:
+    """Highlight the keywords in the text with the specified color."""
+    color = {
+        "name": "cyan",
+        "fail": "red",
+        "info": "blue",
+        "succ": "green",
+    }.get(style, "blue")
+    return f"[{color}]{text}[/{color}]"
