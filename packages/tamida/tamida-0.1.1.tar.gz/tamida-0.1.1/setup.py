4a46c656ea3e28871296b2f241a431751373310b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:35:42 +0000

    Add package tamida/0.1.1/tamida-0.1.1.tar.gz

---
+++ packages/tamida/tamida-0.1.1.tar.gz/tamida-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tamida",  # Tên thư viện trên PyPI
+    version="0.1.1",  # Phiên bản
+    author="Đặng Minh Tài",
+    author_email="dmt826321@gmail.com",
+    description="Một thư viện toán học hỗ trợ số chính phương, số nguyên tố, giai thừa, ...",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dangminhtai/", 
+    packages=find_packages(),
+    install_requires=[
+        "scipy"  # Thêm các thư viện cần thiết
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
