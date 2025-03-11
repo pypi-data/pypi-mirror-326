808080342c6e418dddf7922696ba9c6f208b4cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:35:39 +0000

    Add package poetry-versions-plugin/0.8.0/poetry_versions_plugin-0.8.0.tar.gz

---
+++ packages/poetry-versions-plugin/poetry_versions_plugin-0.8.0.tar.gz/poetry_versions_plugin-0.8.0/poetry_versions_plugin/versions.py
@@ -0,0 +1,12 @@
+# THIS FILE IS GENERATED DURING PROJECT BUILD
+# See poetry poetry-versions-plugin for details
+
+branch = 'release/0.8.0'
+commit = '16dbf86'
+commit_count = 79
+is_dirty = False
+datetime = '2025-02-08 15:44:19'
+version = '0.8.0'
+
+full_version = f'{version}.{branch}+{commit_count}.{commit}'
+# END OF GENERATED CODE
