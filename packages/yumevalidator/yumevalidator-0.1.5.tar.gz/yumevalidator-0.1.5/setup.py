d7863eaf20924f585eb1230a903f4190706a74f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:04:15 +0000

    Add package yumevalidator/0.1.5/yumevalidator-0.1.5.tar.gz

---
+++ packages/yumevalidator/yumevalidator-0.1.5.tar.gz/yumevalidator-0.1.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yumevalidator",  # Change to your package name
+    version="0.1.5",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['public/*'],
+    },
+    install_requires=[
+        'readchar',
+        'smolagents',
+        'selenium',
+        'helium',
+        'pillow'
+    ],  # Add dependencies if needed
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "yumevalidator=src.main:main", # Command name -> Function to run
+        ],
+    },
+    author="Victor Mak",
+    author_email="contact@htetaung.com",
+    description="A GenAI-based UI Validation tool for Deriv AI Hackathon in 2025 Feburary",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yumevalidator/cli",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
