61e8dea10b0e3d778ea28b3e987ad9d5526c6bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:35:41 +0000

    Add package vinclient-petstore-ti/0.0.1/vinclient_petstore_ti-0.0.1.tar.gz

---
+++ packages/vinclient-petstore-ti/vinclient_petstore_ti-0.0.1.tar.gz/vinclient_petstore_ti-0.0.1/vinclient_petstore_ti/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from vinclient_petstore_ti.api.pet_api import PetApi
+from vinclient_petstore_ti.api.store_api import StoreApi
+from vinclient_petstore_ti.api.user_api import UserApi
+
