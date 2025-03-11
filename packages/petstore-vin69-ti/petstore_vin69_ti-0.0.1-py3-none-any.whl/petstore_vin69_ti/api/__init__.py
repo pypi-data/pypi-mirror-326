adf016ed1d1db24886990d39da829bf27938dce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:25:02 +0000

    Add package petstore-vin69-ti/0.0.1/petstore_vin69_ti-0.0.1-py3-none-any.whl

---
+++ packages/petstore-vin69-ti/petstore_vin69_ti-0.0.1-py3-none-any.whl/petstore_vin69_ti/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from petstore_vin69_ti.api.pet_api import PetApi
+from petstore_vin69_ti.api.store_api import StoreApi
+from petstore_vin69_ti.api.user_api import UserApi
+
