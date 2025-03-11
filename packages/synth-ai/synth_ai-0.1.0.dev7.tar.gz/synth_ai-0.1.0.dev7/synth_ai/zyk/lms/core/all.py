6dd991f72d9b8197986ad5a05e763b0a6e787bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:25:06 +0000

    Add package synth-ai/0.1.0.dev7/synth_ai-0.1.0.dev7.tar.gz

---
+++ packages/synth-ai/synth_ai-0.1.0.dev7.tar.gz/synth_ai-0.1.0.dev7/synth_ai/zyk/lms/core/all.py
@@ -0,0 +1,35 @@
+from synth_ai.zyk.lms.vendors.core.anthropic_api import AnthropicAPI
+from synth_ai.zyk.lms.vendors.core.gemini_api import GeminiAPI
+from synth_ai.zyk.lms.vendors.core.openai_api import (
+    OpenAIPrivate,
+    OpenAIStructuredOutputClient,
+)
+from synth_ai.zyk.lms.vendors.supported.deepseek import DeepSeekAPI
+from synth_ai.zyk.lms.vendors.supported.together import TogetherAPI
+
+
+class OpenAIClient(OpenAIPrivate):
+    def __init__(self, synth_logging: bool = True):
+        super().__init__(
+            synth_logging=synth_logging,
+        )
+
+
+class AnthropicClient(AnthropicAPI):
+    def __init__(self):
+        super().__init__()
+
+
+class GeminiClient(GeminiAPI):
+    def __init__(self):
+        super().__init__()
+
+
+class DeepSeekClient(DeepSeekAPI):
+    def __init__(self):
+        super().__init__()
+
+
+class TogetherClient(TogetherAPI):
+    def __init__(self):
+        super().__init__()
