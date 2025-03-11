bec64dff808e27701defb6e70531371a2507ed20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:35:41 +0000

    Add package jkapis/0.1.0/jkapis-0.1.0.tar.gz

---
+++ packages/jkapis/jkapis-0.1.0.tar.gz/jkapis-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jkapis",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here
+    author="Jeyakumar Chinnamuthu",
+    author_email="jchinnamuthu@gmail.com",
+    description="A sample Python package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jchinnamuthu/jkapis",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
---
+++ packages/jkapis/jkapis-0.1.0.tar.gz/jkapis-0.1.0/tests/test_module.py
@@ -0,0 +1,4 @@
+from jkapis.module import hello
+
+def test_hello():
+    assert hello() == "Hello, PyPI!"
