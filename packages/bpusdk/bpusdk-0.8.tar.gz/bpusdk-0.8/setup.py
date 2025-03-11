ad9667741e05437231c3f7379ec28ee23b6c922d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:35:42 +0000

    Add package bpusdk/0.8/bpusdk-0.8.tar.gz

---
+++ packages/bpusdk/bpusdk-0.8.tar.gz/bpusdk-0.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bpusdk',
+    version='0.8',
+    license='MIT',
+    author="GDIIST",
+    author_email='739503445@qq.com',
+    packages=find_packages(),  # Include all packages inside 'src'
+    install_requires=['brainpy>=2.4.2','brainpylib'],
+)
+
