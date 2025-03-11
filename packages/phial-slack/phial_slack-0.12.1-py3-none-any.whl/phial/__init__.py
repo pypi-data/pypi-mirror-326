891190b182e142bae3c05bcf03a66477934cc278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:04:08 +0000

    Add package phial-slack/0.12.1/phial_slack-0.12.1-py3-none-any.whl

---
+++ packages/phial-slack/phial_slack-0.12.1-py3-none-any.whl/phial/__init__.py
@@ -0,0 +1,17 @@
+"""phial Slack Bot."""
+
+from phial.bot import Phial
+from phial.globals import command
+from phial.scheduler import Schedule
+from phial.wrappers import Attachment, Message, PhialResponse, Response
+
+__version__ = "0.12.1"
+__all__ = [
+    "Attachment",
+    "Message",
+    "Phial",
+    "PhialResponse",
+    "Response",
+    "Schedule",
+    "command",
+]
