ecb495694438eae8818e6de9620cfe117f0740ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:25:05 +0000

    Add package wecom-bot-svr/0.3.3/wecom_bot_svr-0.3.3-py3-none-any.whl

---
+++ packages/wecom-bot-svr/wecom_bot_svr-0.3.3-py3-none-any.whl/wecom_bot_svr/__init__.py
@@ -0,0 +1,8 @@
+from .app import WecomBotServer
+from .rsp_msg import RspMsg, RspTextMsg, RspMarkdownMsg
+from .req_msg import ReqMsg
+
+__author__ = "Pan Zhongxian(panzhongxian0532@gmail.com)"
+__license__ = "MIT"
+
+__all__ = ["WecomBotServer", "RspMsg", "ReqMsg", "RspTextMsg", "RspMarkdownMsg"]
